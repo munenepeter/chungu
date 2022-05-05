786cd3314819532d54008539646c0b96fda154bd
munenepeter <peternjega7@gmail.com>
2022-05-05 13:57:03 +0300

    Done with Notification

--- Views/base.view.php
+++ Views/base.view.php
@@ -22,12 +22,23 @@ use Chungu\Core\Mantle\Request; ?>
     <title>Clive <?= (Request::uri() == "") ? "" : "| " . ucwords(str_replace("/", " ", Request::uri())); ?></title>
 </head>
 
-<body class=" bg-gray-50">
+<body class="">
+    <!-- <?php // $notification = "Demo notification For primary navigation and marketing sections, designed to be rendered at 24x24."; ?> -->
 
     <?php if (!empty($notification)) : ?>
-        <div class="fixed bottom-4 right-4 xl:right-20">
-            <span class="transform duration-500 ease-in-out animate-bounce bg-blue-400 px-4 py-3 font-mono font-semibold rounded-lg shadow hover:shadow-xl flex justify-between items-center gap-4">
-                <?= $notification; ?>
-            </span>
+        <div class="fixed bottom-0 right-0 right-0 xl:right-20">
+            <div id="toast-default" class="flex items-center w-full max-w-xs p-4 text-green-700 bg-green-100 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+                <div class="ml-3 text-sm font-normal"><span><?= $notification; ?></span></div>
+             
+                <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-pink-550 text-green-700 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
+                    <span class="sr-only">Close</span>
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+            </div>
+
+
+
         </div>
     <?php endif; ?>

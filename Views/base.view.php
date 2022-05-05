0f46072b6178720a6d3ded2c2142f6a6f65eb561
munenepeter <peternjega7@gmail.com>
2022-05-05 19:09:11 +0300

    second part

--- Views/base.view.php
+++ Views/base.view.php
@@ -18,7 +18,7 @@ use Chungu\Core\Mantle\Request; ?>
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" integrity="sha512-nIwdJlD5/vHj23CbO2iHCXtsqzdTTx3e3uAmpTm4x2Y8xCIFyWu4cSIV8GaGe2UNVq86/1h9EgUZy7tn243qdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script defer src="../static/js/alpine.js"></script>
     <script defer src="../static/js/index.js"></script>
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Chungu <?= (Request::uri() == "") ? "" : "| " . ucwords(str_replace("/", " ", Request::uri())); ?></title>
 </head>
 
--- Views/index.view.php
+++ Views/index.view.php
@@ -3,35 +3,72 @@ include_once 'base.view.php';
 include_once 'sections/nav.view.php'
 ?>
 
-<div class="mt-4 grid grid-cols-4 gap-4">
-    <div class="">
-        <img style="margin-top: 256px;" src="../static/imgs/rotated-right.png" class="mt-4 h-72 w-64" alt="" srcset="">
-    </div>
-    <div class="col-span-2 mx-auto">
-        <div class="max-w-sm bg-white rounded-lg">
-            <a href="#">
-                <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Seasonal offer</h5>
-            </a>
-            <a class="" href="#">
-                <img class="rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
-            </a>
-            <div class="mt-8">
-                <p class="mb-8 font-semibold text-gray-700 dark:text-gray-400">Get our featured seasonal offer of a Diamante Gift set Box at 20% off this season. The Box will come packaged with a set of earrings.</p>
-                <h5 class="mb-8 text-xl font-bold tracking-tight text-pink-550 dark:text-white">Offer valid until 31st May 2022</h5>
-            </div>
-            <div class=" items-center flex justify-between">
-                <a href="#" class="py-3 px-6 text-sm font-medium text-center text-white bg-green-550 rounded-lg hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800">
-                    Add To Cart
-                </a>
-                <a href="#" class="py-3 px-6 text-sm font-medium text-center text-green-550 rounded-lg focus:ring-4 hover:bg-green-600 focus:outline-none focus:ring-green-300 border border-2 border-green-550">
-                    Learn More
-                </a>
-            </div>
+<section id="top-part">
+    <div class="mt-4 grid grid-cols-4 gap-4">
+        <div class="">
+            <img style="margin-top: 256px;" src="../static/imgs/rotated-right.png" class="mt-4 h-72 w-64" alt="" srcset="">
         </div>
+        <div class="col-span-2 mx-auto">
+            <div class="max-w-sm bg-white rounded-lg">
+                <a href="#">
+                    <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Seasonal offer</h5>
+                </a>
+                <a class="" href="#">
+                    <img class="rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
+                </a>
+                <div class="mt-8">
+                    <p class="mb-8 font-semibold text-gray-700 dark:text-gray-400">Get our featured seasonal offer of a Diamante Gift set Box at 20% off this season. The Box will come packaged with a set of earrings.</p>
+                    <h5 class="mb-8 text-xl font-bold tracking-tight text-pink-550 dark:text-white">Offer valid until 31st May 2022</h5>
+                </div>
+                <div class=" items-center flex justify-between">
+                    <a href="#" class="py-3 px-6 text-sm font-medium text-center text-white bg-green-550 rounded-lg hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800">
+                        Add To Cart
+                    </a>
+                    <a href="#" class="py-3 px-6 text-sm font-medium text-center text-green-550 rounded-lg focus:ring-4 hover:bg-green-600 focus:outline-none focus:ring-green-300 border border-2 border-green-550">
+                        Learn More
+                    </a>
+                </div>
+            </div>
 
 
+        </div>
+        <div class="my-8">
+            <img style="float:right; margin-top: 256px;" class="mt-4 h-72 w-64" src="../static/imgs/olive.png" alt="" srcset="">
+        </div>
     </div>
-    <div class="my-4">
-        <img style="float:right; margin-top: 256px;" class="mt-4 h-72 w-64" src="../static/imgs/olive.png" alt="" srcset="">
+</section>
+
+<section id="second-part">
+    <center>
+        <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mt-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Shop by Category</h5>
+    </center>
+    <div class="p-4 mt-8 flex flex-wrap mx-auto text-center justify-center items-center space-x-8">
+        <div class="mr-4"><svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-green-550" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
+            </svg></div>
+        <div class="block">
+            <div class="mx-auto w-48 h-48 rounded-full bg-red-700"> <img class="rounded-full h-full w-full" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" srcset=""></div>
+            <h5 class="mt-8 text-xl font-bold tracking-tight text-pink-550 dark:text-white">Rings</h5>
+        </div>
+        <div class="block">
+            <div class="mx-auto w-48 h-48 rounded-full bg-red-700"> <img class="rounded-full h-full w-full" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" srcset=""></div>
+            <h5 class="mt-8 text-xl font-bold tracking-tight text-pink-550 dark:text-white">Necklaces</h5>
+        </div>
+        <div class="block">
+            <div class="mx-auto w-48 h-48 rounded-full bg-red-700"> <img class="rounded-full h-full w-full" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" srcset=""></div>
+            <h5 class="mt-8 text-xl font-bold tracking-tight text-pink-550 dark:text-white">Earrings</h5>
+        </div>
+        <div class="block">
+            <div class="mx-auto w-48 h-48 rounded-full bg-red-700"> <img class="rounded-full h-full w-full" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" srcset=""></div>
+            <h5 class="mt-8 text-xl font-bold tracking-tight text-pink-550 dark:text-white">Bracelets</h5>
+        </div>
+        <div class="ml-4"><svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-green-550" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg></div>
     </div>
-</div>
+    <div class="mx-auto">
+        <a href="#" class="py-3 px-6 text-sm font-medium text-center text-green-550 rounded-lg focus:ring-4 hover:bg-green-600 focus:outline-none focus:ring-green-300 border border-2 border-green-550">
+           View More Items
+        </a>
+    </div>
+</section>

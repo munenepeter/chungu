68f200ef74ed8896268abf098f7ba952efb98194
munenepeter <peternjega7@gmail.com>
2022-05-05 11:55:43 +0300

    added my static assets

--- Views/base.view.php
+++ Views/base.view.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-    <link rel="icon" href="../static/imgs/icons/favicon.svg" type="image/gif">
+    <link rel="icon" href="../static/imgs/favicon.svg" type="image/gif">
     <link rel="stylesheet" type="text/css" href="../static/css/tailwind.css">
 
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" integrity="sha512-nIwdJlD5/vHj23CbO2iHCXtsqzdTTx3e3uAmpTm4x2Y8xCIFyWu4cSIV8GaGe2UNVq86/1h9EgUZy7tn243qdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>

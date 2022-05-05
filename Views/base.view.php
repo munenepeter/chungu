4e8d7a9db5fe678b0475576606f8a297124d2a2a
munenepeter <peternjega7@gmail.com>
2022-05-05 12:00:26 +0300

    added js & assets for dev

--- Views/base.view.php
+++ Views/base.view.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <link rel="icon" href="../static/imgs/favicon.svg" type="image/gif">
     <link rel="stylesheet" type="text/css" href="../static/css/tailwind.css">
-
+    <script src="https://unpkg.com/flowbite@1.4.5/dist/flowbite.js"></script>
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" integrity="sha512-nIwdJlD5/vHj23CbO2iHCXtsqzdTTx3e3uAmpTm4x2Y8xCIFyWu4cSIV8GaGe2UNVq86/1h9EgUZy7tn243qdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script defer  src="../static/js/alpine.js"></script>
     <script defer  src="../static/js/index.js"></script>

8c86aff6b34ee5b906d54b0e1818c25efb9628cd
munenepeter <peternjega7@gmail.com>
2022-05-05 15:41:24 +0300

    fixing the olive imgs

--- Views/index.view.php
+++ Views/index.view.php
@@ -2,3 +2,35 @@
 include_once 'base.view.php';
 include_once 'sections/nav.view.php'
 ?>
+
+<div class="mt-4 grid grid-cols-4 gap-4">
+    <div class="">
+         <img style="transform: rotate(74.142rad) translateX(10px);" src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/1631949/olive-branch-clipart-md.png" alt="" srcset=""></div>
+    <div class="col-span-2 mx-auto">
+        <div class="max-w-sm bg-white rounded-lg">
+            <a href="#">
+                <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Seasonal offer</h5>
+            </a>
+            <a class="" href="#">
+                <img class="rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
+            </a>
+            <div class="mt-8">
+                <p class="mb-8 font-semibold text-gray-700 dark:text-gray-400">Get our featured seasonal offer of a Diamante Gift set Box at 20% off this season. The Box will come packaged with a set of earrings.</p>
+                <h5 class="mb-8 text-xl font-bold tracking-tight text-pink-550 dark:text-white">Offer valid until 31st May 2022</h5>
+            </div>
+            <div class=" items-center flex justify-between">
+                <a href="#" class="py-3 px-6 text-sm font-medium text-center text-white bg-green-550 rounded-lg hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800">
+                    Add To Cart
+                </a>
+                <a href="#" class="py-3 px-6 text-sm font-medium text-center text-green-550 rounded-lg focus:ring-4 hover:bg-green-600 focus:outline-none focus:ring-green-300 border border-2 border-green-550">
+                    Learn More
+                </a>
+            </div>
+        </div>
+
+
+    </div>
+    <div class="">
+    <img style="transform: rotate(3.142rad) translateX(-180px); " src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/1631949/olive-branch-clipart-md.png" alt="" srcset="">
+    </div>
+</div>

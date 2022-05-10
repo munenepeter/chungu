49de6692c01a9d2832bcd68dc9e6ee5a168b4069
munenepeter <peternjega7@gmail.com>
2022-05-10 19:07:05 +0300

    poppins font

---
+++ Views/product.view.php
@@ -0,0 +1,19 @@
+<div class="col-span-2 mx-auto">
+    <div class="max-w-sm bg-white rounded-lg">
+        <a class="" href="#">
+            <img class="w-32 h-32 rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
+        </a>
+        <div class="mt-8">
+            <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
+            <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Ksh 200.00</p>
+            <p class="mb-8 font-semibold text-gray-700 dark:text-gray-400">Beautiful earrings for every occassion</p>
+            <h5 class="mb-4 text-xl font-semibold tracking-tight text-pink-550 dark:text-white">Available colors</h5>
+        </div>
+        <div class="items-center flex justify-start space-x-2">
+            <div class="w-5 h-5 rounded-full bg-pink-550"></div>
+            <div class="w-5 h-5 rounded-full bg-green-550"></div>         </div>
+        <a href="#" class="lg:py-3 px-3  md:px-6 text-sm font-medium text-center text-white bg-green-550 rounded-lg hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800">
+            Add To Cart
+        </a>
+    </div>
+</div>
--- Views/shop.view.php
+++ Views/shop.view.php
@@ -3,35 +3,95 @@ include_once 'base.view.php';
 include_once 'sections/nav.view.php'
 ?>
 
-<section id="top-part">
-    <div class="mt-4 grid grid-cols-4 gap-4">
-        <div class="my-8">
-            <img src="../static/imgs/rotated-right.png" class="mt-72 h-20 md:h-28 lg:h-72 w-64" alt="" srcset="">
-        </div>
-        <div class="col-span-2 mx-auto">
-            <div class="max-w-sm bg-white rounded-lg">
-                <a href="#">
-                    <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Seasonal offer</h5>
-                </a>
-                <a class="" href="#">
-                    <img class="rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
-                </a>
-                <div class="mt-8">
-                    <p class="mb-8 font-semibold text-gray-700 dark:text-gray-400">Get our featured seasonal offer of a Diamante Gift set Box at 20% off this season. The Box will come packaged with a set of earrings.</p>
-                    <h5 class="mb-8 text-xl font-bold tracking-tight text-pink-550 dark:text-white">Offer valid until 31st May 2022</h5>
-                </div>
-                <div class="items-center flex justify-between space-x-2">
-                    <a href="#" class="py-2 lg:py-3 px-3  md:px-6 text-sm font-medium text-center text-white bg-green-550 rounded-lg hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800">
-                        Add To Cart
+
+<div class="p-8">
+<section id="Earrings">
+    <a href="#">
+        <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Earrings</h5>
+    </a>
+    <div class="mt-4 grid md:grid-cols-2 lg:grid-cols-4 gap-2  lg:gap-4">
+
+        <?php for ($i = 0; $i < 4; $i++) : ?>
+            <div class="mx-auto">
+                <div class="max-w-sm bg-white rounded-lg">
+                    <a class="" href="#">
+                        <img class="transform transition duration-500 hover:scale-125w-48 h-32 rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
                     </a>
-                    <a href="#" class="py-2 lg:py-3 px-3 md:px-6 text-sm font-medium text-center text-green-550 rounded-lg focus:ring-4 hover:bg-green-500 hover:text-white focus:outline-none focus:ring-green-300 border border-2 border-green-550">
-                        Learn More
-                    </a>
+                    <div class="mt-8">
+                        <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
+                        <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Ksh 200.00</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="my-8">
-            <img style="float:right;" class="mt-72 h-20 md:h-28 lg:h-72 w-64" src="../static/imgs/olive.png" alt="" srcset="">
-        </div>
+        <?php endfor; ?>
+
+
     </div>
 </section>
+
+<section id="necklaces">
+    <a href="#">
+        <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Necklaces</h5>
+    </a>
+    <div class="mt-4 grid md:grid-cols-2 lg:grid-cols-4 gap-2  lg:gap-4">
+
+        <?php for ($i = 0; $i < 4; $i++) : ?>
+            <div class="mx-auto">
+                <div class="max-w-sm bg-white rounded-lg">
+                    <a class="" href="#">
+                        <img class="w-48 h-32 rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
+                    </a>
+                    <div class="mt-8">
+                        <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
+                        <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Ksh 200.00</p>
+                    </div>
+                </div>
+            </div>
+        <?php endfor; ?>
+    </div>
+</section>
+
+<section id="Bracelets">
+    <a href="#">
+        <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Bracelets</h5>
+    </a>
+    <div class="mt-4 grid md:grid-cols-2 lg:grid-cols-4 gap-2  lg:gap-4">
+
+        <?php for ($i = 0; $i < 4; $i++) : ?>
+            <div class="mx-auto">
+                <div class="max-w-sm bg-white rounded-lg">
+                    <a class="" href="#">
+                        <img class="w-48 h-32 rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
+                    </a>
+                    <div class="mt-8">
+                        <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
+                        <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Ksh 200.00</p>
+                    </div>
+                </div>
+            </div>
+        <?php endfor; ?>
+    </div>
+</section>
+
+<section id="Anklets">
+    <a href="#">
+        <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Anklets</h5>
+    </a>
+    <div class="mt-4 grid md:grid-cols-2 lg:grid-cols-4 gap-2  lg:gap-4">
+
+        <?php for ($i = 0; $i < 4; $i++) : ?>
+            <div class="mx-auto">
+                <div class="max-w-sm bg-white rounded-lg">
+                    <a class="" href="#">
+                        <img class="w-48 h-32 rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
+                    </a>
+                    <div class="mt-8">
+                        <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
+                        <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Ksh 200.00</p>
+                    </div>
+                </div>
+            </div>
+        <?php endfor; ?>
+    </div>
+</section>
+</div>

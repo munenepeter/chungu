42d4d2b30117d98cfed4ab90f1322b33954cccc3
munenepeter <peternjega7@gmail.com>
2022-05-13 18:03:55 +0300

    Added singularize helper

--- Core/helpers.php
+++ Core/helpers.php
@@ -93,3 +93,74 @@ function auth() {
     }
     return Session::get('loggedIn');
 }
+
+/**
+ * singularize
+ * This returns the singular version of common english words
+ * --from https://www.kavoir.com/2011/04/php-class-converting-plural-to-singular-or-vice-versa-in-english.html
+ * 
+ * @param string $phrase the word to be pluralised
+ * @param int $value 
+ * 
+ * @return string plural 
+ */
+
+function singularize($word) {
+    $singular = array(
+        '/(quiz)zes$/i' => '\1',
+        '/(matr)ices$/i' => '\1ix',
+        '/(vert|ind)ices$/i' => '\1ex',
+        '/^(ox)en/i' => '\1',
+        '/(alias|status)es$/i' => '\1',
+        '/([octop|vir])i$/i' => '\1us',
+        '/(cris|ax|test)es$/i' => '\1is',
+        '/(shoe)s$/i' => '\1',
+        '/(o)es$/i' => '\1',
+        '/(bus)es$/i' => '\1',
+        '/([m|l])ice$/i' => '\1ouse',
+        '/(x|ch|ss|sh)es$/i' => '\1',
+        '/(m)ovies$/i' => '\1ovie',
+        '/(s)eries$/i' => '\1eries',
+        '/([^aeiouy]|qu)ies$/i' => '\1y',
+        '/([lr])ves$/i' => '\1f',
+        '/(tive)s$/i' => '\1',
+        '/(hive)s$/i' => '\1',
+        '/([^f])ves$/i' => '\1fe',
+        '/(^analy)ses$/i' => '\1sis',
+        '/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$/i' => '\1\2sis',
+        '/([ti])a$/i' => '\1um',
+        '/(n)ews$/i' => '\1ews',
+        '/s$/i' => '',
+    );
+
+    $uncountable = array('equipment', 'information', 'rice', 'money', 'species', 'series', 'fish', 'sheep');
+
+    $irregular = array(
+        'person' => 'people',
+        'man' => 'men',
+        'child' => 'children',
+        'sex' => 'sexes',
+        'move' => 'moves'
+    );
+
+    $lowercased_word = strtolower($word);
+    foreach ($uncountable as $_uncountable) {
+        if (substr($lowercased_word, (-1 * strlen($_uncountable))) == $_uncountable) {
+            return $word;
+        }
+    }
+
+    foreach ($irregular as $_plural => $_singular) {
+        if (preg_match('/(' . $_singular . ')$/i', $word, $arr)) {
+            return preg_replace('/(' . $_singular . ')$/i', substr($arr[0], 0, 1) . substr($_plural, 1), $word);
+        }
+    }
+
+    foreach ($singular as $rule => $replacement) {
+        if (preg_match($rule, $word)) {
+            return preg_replace($rule, $replacement, $word);
+        }
+    }
+
+    return $word;
+}

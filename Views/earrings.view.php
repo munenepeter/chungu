5b5de00e047cd5e45f117f68efa0a3e8d9cc2e6b
munenepeter <peternjega7@gmail.com>
2022-05-19 15:30:16 +0300

    working with an idea

--- Views/earrings.view.php
+++ Views/earrings.view.php
@@ -47,7 +47,54 @@ include_once 'sections/nav.view.php'
             </div>
         </div>
 
-
-
+        <a class="py-4" href="/shop/earrings">
+            <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Related Earrings</h5>
+        </a>
+        <div class="flex space-x-4">
+        <?php for ($i = 0; $i < 3; $i++) : ?>
+            <div class="mx-auto">
+                <div class="max-w-sm bg-white rounded-lg">
+                    <a class="" href="/shop/earrings?pro_id=8hcjd9">
+                        <img class="object-cover transform transition duration-500 hover:scale-125  w-48 h-32 rounded-lg " src="../static/imgs/earrings/fancy-beads.jpg" alt="">
+                    </a>
+                    <div class="mt-8">
+                        <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
+                        <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Ksh 200.00</p>
+                    </div>
+                </div>
+            </div>
+        <?php endfor; ?>
+        </div>
     </div>
+<?php else : ?>
+
+    <main class="p-8">
+        <section id="Earrings">
+            <center>
+                <a class="py-4" href="/shop/earrings">
+                    <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Earrings</h5>
+                </a>
+            </center>
+            <div class="mt-4 grid md:grid-cols-2 lg:grid-cols-8 gap-2  lg:gap-4">
+                <div class="col-span-1"></div>
+                <?php for ($i = 0; $i < 7; $i++) : ?>
+                    <div class="col-span-2 mx-auto">
+                        <div class="max-w-sm bg-white rounded-lg">
+                            <a class="" href="/shop/earrings?pro_id=8hcjd9">
+                                <img class="object-cover transform transition duration-500 hover:scale-125  w-48 h-32 rounded-lg " src="../static/imgs/earrings/fancy-beads.jpg" alt="">
+                            </a>
+                            <div class="mt-8">
+                                <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
+                                <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Ksh 200.00</p>
+                            </div>
+                        </div>
+                    </div>
+                <?php endfor; ?>
+                <div class="col-span-1"></div>
+            </div>
+        </section>
+    </main>
 <?php endif; ?>
+<?php
+include_once 'sections/footer.view.php'
+?>

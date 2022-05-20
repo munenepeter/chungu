0de1733fe7bc1865556a5cf1f2f5162533c4f90f
munenepeter <peternjega7@gmail.com>
2022-05-20 11:27:09 +0300

    swithed to correct uri

--- Views/anklets.view.php
+++ Views/anklets.view.php
@@ -7,13 +7,13 @@ include_once 'sections/nav.view.php'
     <div class="grid place-items-center">
         <div class="">
             <div class="max-w-sm bg-white rounded-lg">
-                <a class="" href="/shop/earrings?pro_id=8hcjd9">
+                <a class="" href="/shop/anklets?pro_id=8hcjd9">
                     <img class="h-48 object-cover transform transition duration-500 hover:scale-125 rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="">
                 </a>
                 <div class="mt-8">
                     <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
                     <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Ksh 200.00</p>
-                    <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Beautiful earrings for every occassion</p>
+                    <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Beautiful anklets for every occassion</p>
                     <h5 class="mb-4 text-xl font-semibold tracking-tight text-pink-550 dark:text-white">Available colors</h5>
                 </div>
                 <div class="items-center flex justify-start mb-2">
@@ -47,15 +47,15 @@ include_once 'sections/nav.view.php'
             </div>
         </div>
 
-        <a class="py-4" href="/shop/earrings">
-            <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Related Earrings</h5>
+        <a class="py-4" href="/shop/anklets">
+            <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Related anklets</h5>
         </a>
         <div class="flex space-x-4">
         <?php for ($i = 0; $i < 3; $i++) : ?>
             <div class="mx-auto">
                 <div class="max-w-sm bg-white rounded-lg">
-                    <a class="" href="/shop/earrings?pro_id=8hcjd9">
-                        <img class="object-cover transform transition duration-500 hover:scale-125  w-48 h-32 rounded-lg " src="../static/imgs/earrings/fancy-beads.jpg" alt="">
+                    <a class="" href="/shop/anklets?pro_id=8hcjd9">
+                        <img class="object-cover transform transition duration-500 hover:scale-125  w-48 h-32 rounded-lg " src="../static/imgs/anklets/fancy-beads.jpg" alt="">
                     </a>
                     <div class="mt-8">
                         <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
@@ -69,10 +69,10 @@ include_once 'sections/nav.view.php'
 <?php else : ?>
 
     <main class="p-8">
-        <section id="Earrings">
+        <section id="anklets">
             <center>
-                <a class="py-4" href="/shop/earrings">
-                    <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Earrings</h5>
+                <a class="py-4" href="/shop/anklets">
+                    <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">anklets</h5>
                 </a>
             </center>
             <div class="mt-4 grid md:grid-cols-2 lg:grid-cols-8 gap-2  lg:gap-4">
@@ -80,8 +80,8 @@ include_once 'sections/nav.view.php'
                 <?php for ($i = 0; $i < 7; $i++) : ?>
                     <div class="col-span-2 mx-auto">
                         <div class="max-w-sm bg-white rounded-lg">
-                            <a class="" href="/shop/earrings?pro_id=8hcjd9">
-                                <img class="object-cover transform transition duration-500 hover:scale-125  w-48 h-32 rounded-lg " src="../static/imgs/earrings/fancy-beads.jpg" alt="">
+                            <a class="" href="/shop/anklets?pro_id=8hcjd9">
+                                <img class="object-cover transform transition duration-500 hover:scale-125  w-48 h-32 rounded-lg " src="../static/imgs/anklets/fancy-beads.jpg" alt="">
                             </a>
                             <div class="mt-8">
                                 <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>

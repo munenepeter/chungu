fb7a2435c83f799c03de418b88f31f5a9c34880a
munenepeter <peternjega7@gmail.com>
2022-05-20 14:26:39 +0300

    statistics done

--- Views/dashboard.view.php
+++ Views/dashboard.view.php
@@ -2,3 +2,72 @@
 include_once 'base.view.php';
 include_once 'sections/nav.view.php'
 ?>
+
+<div class="flex items-center justify-center m-2">
+
+    <!-- Component Start -->
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <div class=" w-32 md:w-48 bg-white shadow-2xl p-6 rounded-2xl">
+            <div class="flex items-center">
+                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-pink-100">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 stroke-current text-pink-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </span>
+                <span class="ml-2 text-sm font-medium text-pink-550">Earrings</span>
+            </div>
+            <span class="block text-4xl font-semibold mt-4 text-green-550">320</span>
+            <div class="flex text-xs mt-3 font-medium">
+                <span class="text-green-500">78</span>
+                <span class="ml-1 text-pink-550">Available</span>
+            </div>
+        </div>
+        <div class="w-32 md:w-48 bg-white shadow-2xl p-6 rounded-2xl">
+            <div class="flex items-center">
+                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-red-100">
+                    <svg class="w-4 h-4 stroke-current text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+                </span>
+                <span class="ml-2 text-sm font-medium text-pink-550">Necklaces</span>
+            </div>
+            <span class="block text-4xl font-semibold mt-4 text-green-550">45</span>
+            <div class="flex text-xs mt-3 font-medium">
+                <span class="text-green-500">14</span>
+                <span class="ml-1 text-pink-550">Available</span>
+            </div>
+        </div>
+        <div class="w-32 md:w-48 bg-white shadow-2xl p-6 rounded-2xl">
+            <div class="flex items-center">
+                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-red-100">
+                    <svg class="w-4 h-4 stroke-current text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                    </svg>
+                </span>
+                <span class="ml-2 text-sm font-medium text-pink-550">Anklets</span>
+            </div>
+            <span class="block text-4xl font-semibold mt-4 text-green-550">14</span>
+            <div class="flex text-xs mt-3 font-medium">
+                <span class="text-green-500">12</span>
+                <span class="ml-1 text-pink-550">Available</span>
+            </div>
+        </div>
+        <div class="w-32 md:w-48 bg-white shadow-2xl p-6 rounded-2xl">
+            <div class="flex items-center">
+                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-blue-100">
+                    <svg class="w-4 h-4 stroke-current text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                    </svg>
+                </span>
+                <span class="ml-2 text-sm font-medium text-pink-550">Bracelets</span>
+            </div>
+            <span class="block text-4xl font-semibold mt-4 text-green-550">26</span>
+            <div class="flex text-xs mt-3 font-medium">
+                <span class="text-red-500">0</span>
+                <span class="ml-1 text-pink-550">Available</span>
+            </div>
+        </div>
+    </div>
+    <!-- Component End  -->
+
+</div>

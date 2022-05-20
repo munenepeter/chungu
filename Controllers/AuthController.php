09063eb77ad12c41e9d70716be1ef141aee39f5b
munenepeter <peternjega7@gmail.com>
2022-05-20 11:43:15 +0300

    Added validation rules

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -1,6 +1,22 @@
 <?php
+
 namespace Chungu\Controllers;
 
-class AuthController{
+use Chungu\Core\Mantle\Auth;
+use Chungu\Core\Mantle\Request;
+use Chungu\Core\Mantle\Session;
 
+class AuthController {
+    public function login() {
+
+        $username = Request::validate(Request::form('username'));
+        $password = Request::validate(Request::form('password'));
+
+        Auth::login($username, $password);
+    }
+
+    public function logout() {
+        Auth::logout(Session::get('user'));
+        redirect('index');
+    }
 }

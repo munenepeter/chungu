d16fc3097428345b7c2c7c408e65e25c362cf51c
munenepeter <peternjega7@gmail.com>
2022-05-23 15:17:45 +0300

    fixed the height of the sidebar

--- Views/shop.view.php
+++ Views/shop.view.php
@@ -5,17 +5,17 @@ include_once 'sections/nav.view.php'
 
 
 <main class="flex w-full">
-    <aside class="hidden md:block h-screen sticky top-4 -z-50 w-1/4 mt-12 p-6 sm:w-60 text-green-550">
+    <aside class="hidden md:block h-screen sticky top-4 -z-50 w-1/4 mt-16 p-6 sm:w-60 text-green-550">
         <nav class="space-y-8 text-sm">
-            <div class="space-y-2">
-                <div class="flex flex-col space-y-1">
-                    <a rel="noopener noreferrer" href="#">New Arrivals
+            <div class="space-y-4">
+                <div class="flex flex-col space-y-4">
+                    <a class="text-lg" rel="noopener noreferrer" href="#">New Arrivals
                     </a>
-                    <a class="" rel="noopener noreferrer" href="#">Best Sellers</a>
+                    <a class="text-lg" rel="noopener noreferrer" href="#">Best Sellers</a>
                 </div>
             </div>
             <div class="space-y-2">
-                <div class="flex flex-col space-y-1">
+                <div class="flex flex-col space-y-2">
                     <a class="" rel="noopener noreferrer" href="#">Shop All
                     </a>
                     <a class="" rel="noopener noreferrer" href="#">Earrings

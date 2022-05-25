2966614b69a3d1b77531bbbd89910940b3eb7adf
munenepeter <peternjega7@gmail.com>
2022-05-25 11:18:56 +0300

    reded=sign of the bracelets page & fixed alt

--- Views/bracelets.view.php
+++ Views/bracelets.view.php
@@ -50,16 +50,16 @@ include_once 'sections/nav.view.php'
         <a class="py-4" href="/shop/bracelets">
             <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">You May Also Like</h5>
         </a>
-        <div class="flex space-x-4">
-        <?php for ($i = 0; $i < 3; $i++) : ?>
-            <div class="mx-auto">
+        <div class="flex space-x-6">
+        <?php for ($i = 0; $i < 4; $i++) : ?>
+            <div class="mx-auto p-4">
                 <div class="max-w-sm bg-white rounded-lg">
                     <a class="" href="/shop/bracelets?pro_id=8hcjd9">
-                        <img class="object-cover transform transition duration-500 hover:scale-125  w-48 h-32 rounded-lg " src="../static/imgs/bracelets/fancy-beads.jpg" alt="">
+                        <img class="object-cover transform transition duration-500 hover:scale-125  w-48 h-64 rounded-lg " src="../static/imgs/bracelets/fancy-beads.jpg" alt="">
                     </a>
                     <div class="mt-8">
-                        <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
-                        <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Ksh 200.00</p>
+                    <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Chungu Collections</p>
+                        <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
                     </div>
                 </div>
             </div>
@@ -68,29 +68,62 @@ include_once 'sections/nav.view.php'
     </div>
 <?php else : ?>
 
-    <main class="p-8">
-        <section id="bracelets">
-            <center>
+    <main class="flex w-full">
+    <aside class="hidden md:block h-screen sticky top-4 -z-50 w-1/4 mt-16 p-6 sm:w-60 text-green-550">
+        <nav class="space-y-8 text-sm">
+            <div class="space-y-4">
+                <div class="flex flex-col space-y-4">
+                    <a class="text-lg hover:text-pink-550" rel="noopener noreferrer" href="#">New Arrivals
+                    </a>
+                    <a class="text-lg hover:text-pink-550" rel="noopener noreferrer" href="#">Best Sellers</a>
+                </div>
+            </div>
+            <div class="space-y-2">
+                <div class="flex flex-col space-y-2">
+                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Shop All
+                    </a>
+                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">bracelets
+                    </a>
+                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Single bracelets
+                    </a>
+
+                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Rings
+                    </a>
+                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Necklaces + Pendants
+                    </a>
+                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Braceletes
+                    </a>
+                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Anklets
+                    </a>
+                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Wedding</a>
+                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Men's</a>
+                </div>
+            </div>
+
+        </nav>
+    </aside>
+
+        <section id="bracelets" class="md:h-screen md:overflow-y-auto w-full md:overflow-x-hidden">
+        <div class="container px-5 py-6 mx-auto">
+        <center>
                 <a class="py-4" href="/shop/bracelets">
                     <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">bracelets</h5>
                 </a>
             </center>
-            <div class="mt-4 grid md:grid-cols-2 lg:grid-cols-8 gap-2  lg:gap-4">
-                <div class="col-span-1"></div>
-                <?php for ($i = 0; $i < 7; $i++) : ?>
-                    <div class="col-span-2 mx-auto">
-                        <div class="max-w-sm bg-white rounded-lg">
-                            <a class="" href="/shop/bracelets?pro_id=8hcjd9">
-                                <img class="object-cover transform transition duration-500 hover:scale-125  w-48 h-32 rounded-lg " src="../static/imgs/bracelets/fancy-beads.jpg" alt="">
-                            </a>
-                            <div class="mt-8">
-                                <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
-                                <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Ksh 200.00</p>
-                            </div>
+            <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 lg:gap-8 -m-4 sm:p-4">    
+                <?php for ($i = 0; $i < 12; $i++) : ?>
+                    <div class="p-4 w-full">
+                        <a class="block relative h-56 rounded overflow-hidden">
+                            <img alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover  w-full h-full block" src="../static/imgs/bracelets/02.jpeg">
+                        </a>
+                        <div class="mt-4">
+                            <h3 class="text-pink-550 text-xs tracking-widest title-font mb-1 uppercase">Bracelets</h3>
+                            <h2 style="font-family: 'Cedarville Cursive', cursive;" class="text-green-550 title-font text-lg font-medium">Timeless Darlings</h2>
+                            <p class="mt-1 text-pink-550">Ksh200.00</p>
                         </div>
                     </div>
-                <?php endfor; ?>
-                <div class="col-span-1"></div>
+                <?php endfor; ?> 
+            </div>
             </div>
         </section>
     </main>
--- Views/shop.view.php
+++ Views/shop.view.php
@@ -50,7 +50,7 @@ include_once 'sections/nav.view.php'
                 <?php for ($i = 0; $i < 3; $i++) : ?>
                     <div class=" p-4 w-full">
                         <a class="block relative h-64 rounded overflow-hidden">
-                            <img alt="ecommerce" class="transform transition duration-500 hover:scale-125 object-cover  w-full h-full block" src="../static/imgs/earrings/02.jpeg">
+                            <img alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover  w-full h-full block" src="../static/imgs/earrings/02.jpeg">
                         </a>
                         <div class="mt-4">
                             <h3 class="text-pink-550 text-xs tracking-widest title-font mb-1">EARRINGS</h3>
@@ -71,7 +71,7 @@ include_once 'sections/nav.view.php'
                 <?php for ($i = 0; $i < 3; $i++) : ?>
                     <div class="p-4 w-full">
                         <a class="block relative h-64 rounded overflow-hidden">
-                            <img alt="ecommerce" class="transform transition duration-500 hover:scale-125 object-cover  w-full h-full block" src="../static/imgs/earrings/01.jpeg">
+                            <img alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover  w-full h-full block" src="../static/imgs/earrings/01.jpeg">
                         </a>
                         <div class="mt-4">
                             <h3 class="text-pink-550 text-xs tracking-widest title-font mb-1">NECKLACES</h3>
@@ -84,7 +84,7 @@ include_once 'sections/nav.view.php'
         </div>
         <div class="container px-5 py-6 mx-auto">
             <center>
-                <a class="py-4" href="/shop/Braceletes">
+                <a class="py-4" href="/shop/bracelets">
                     <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Braceletes</h5>
                 </a>
             </center>
@@ -92,7 +92,7 @@ include_once 'sections/nav.view.php'
                 <?php for ($i = 0; $i < 3; $i++) : ?>
                     <div class=" p-4 w-full">
                         <a class="block relative h-64 rounded overflow-hidden">
-                            <img alt="ecommerce" class="transform transition duration-500 hover:scale-125 object-cover object-center w-full h-full block" src="../static/imgs/earrings/03.jpeg">
+                            <img alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover object-center w-full h-full block" src="../static/imgs/earrings/03.jpeg">
                         </a>
                         <div class="mt-4">
                             <h3 class="text-pink-550 text-xs tracking-widest title-font mb-1 uppercase">Braceletes</h3>
@@ -113,7 +113,7 @@ include_once 'sections/nav.view.php'
                 <?php for ($i = 0; $i < 3; $i++) : ?>
                     <div class=" p-4 w-full">
                         <a class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="transform transition duration-500 hover:scale-125 object-cover object-center w-full h-full block" src="../static/imgs/earrings/fancy-beads.jpg">
+                            <img alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover object-center w-full h-full block" src="../static/imgs/earrings/fancy-beads.jpg">
                         </a>
                         <div class="mt-4">
                             <h3 class="text-pink-550 text-xs tracking-widest title-font mb-1 uppercase">Anklets</h3>

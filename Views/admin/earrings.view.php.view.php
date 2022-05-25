63ac54a7ef44ccf2ff55827857920862bafd469e
munenepeter <peternjega7@gmail.com>
2022-05-25 13:13:58 +0300

    corrected the paths

--- Views/admin/earrings.view.php.view.php
+++ Views/admin/earrings.view.php.view.php
@@ -1,6 +1,6 @@
 <?php 
-include_once 'base.view.php';
-include_once 'sections/admin-nav.view.php' ?>
+include_once '../base.view.php';
+include_once '../sections/admin-nav.view.php' ?>
 
 <div class="grid place-items-center">
        Created earring-admin.view.php's view; please edit

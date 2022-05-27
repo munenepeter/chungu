aac7a8611823adab99b941370a0bfd6d82228108
munenepeter <peternjega7@gmail.com>
2022-05-27 12:09:42 +0300

    Logger in Auth

--- Core/Mantle/Auth.php
+++ Core/Mantle/Auth.php
@@ -13,22 +13,26 @@ class Auth {
         $password = md5($password);
         $user = User::where(['username', 'password',], ['username', $username]);
         if (empty($user)) {
+            Logger::log("INFO: Login: no account with {$username} username");
             array_push(Request::$errors, "There is no account with {$username} username");
             view('signin', ['e' => Request::$errors]);
             return;
         }
         if ($password === $user[0]->password) {
+            Logger::log("INFO: Login: Successfully logged in {$username}");
             Session::make('loggedIn', true);
             Session::make('user', $user[0]->username);
             //Todo Implement Session tokens  
             redirect('/dashboard');
         } else {
+            Logger::log("INFO: Login: Wrong Credentials");
             array_push(Request::$errors, "Wrong credentials, Please try again!");
             view('login', ['e' => Request::$errors]);
             return;
         }
     }
     public static function logout($user) {
+        Logger::log("INFO: Login: logged out $user");
         Session::unset($user);
         Session::make('loggedIn', false);
         Session::destroy();

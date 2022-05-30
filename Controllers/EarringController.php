bd66c655fe891e8ee7d3cd90437828d4bea3ddfc
munenepeter <peternjega7@gmail.com>
2022-05-30 19:33:06 +0300

    an api for upload

--- Controllers/EarringController.php
+++ Controllers/EarringController.php
@@ -14,6 +14,8 @@ class EarringController {
     public function addearrings() {
         $uploadlocation = '/static/imgs/earrings';
         $image = "path-to_dummy";
+
+        // upload the file
         if (!empty($_FILES['image'])) {
 
             $upload = Upload::factory($uploadlocation);
@@ -27,27 +29,38 @@ class EarringController {
             $upload->set_allowed_mime_types(['image/jpeg', 'image/png']);
 
             $results = $upload->upload();
-         
-           $image = $results['path'];
-        }
-     
-        $request =  new Request();
 
+            $image = $results['path'];
+        }
+
+        /*
+        $request->upload(
+            $_FILES['images'],
+             path, 
+             max, 
+             allowed mime
+        );
+        
+        */
+
+
+        $request =  new Request();
+        //validate the input
         $request->validate($_POST, [
             'name' => 'required',
             'price' => 'required',
             'quantity' => 'required'
         ]);
-
+        //get the id of earring in the categories table
         $category_id = 1;
-        //ceate product
+        //create product
         Product::create([
-           'id' => uniqid(),
-           'name' => $request->form('name'),
-           'price' => $request->form('price'),
-           'quantity' => $request->form('quantity'),
-           'image' => $image,
-           'category_id' => $category_id
+            'id' => uniqid(),
+            'name' => $request->form('name'),
+            'price' => $request->form('price'),
+            'quantity' => $request->form('quantity'),
+            'image' => $image,
+            'category_id' => $category_id
         ]);
     }
 }

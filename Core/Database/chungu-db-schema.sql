84fc6a56b7297066612125b83c430d1eafc26318
munenepeter <peternjega7@gmail.com>
2022-05-31 11:59:21 +0300

    Added products  table schema

--- Core/Database/chungu-db-schema.sql
+++ Core/Database/chungu-db-schema.sql
@@ -7,14 +7,46 @@ CREATE TABLE `users` (
   `created_at` datetime,
   `updated_at` datetime
 );
-
 --@block
-
-INSERT INTO users (username, email, password, role, created_at, updated_at)
-VALUES(	
-  'admin', 'admin@chungu.co.ke', '81dc9bdb52d04dc20036dbd8313ed055', 'admin', '2022-03-24 09:33:40', '2022-03-24 09:33:40'
-),
-('maeve', 'maeve@chungu.co.ke', '81dc9bdb52d04dc20036dbd8313ed055', 'user' , '2022-03-24 09:33:40', '2022-03-24 09:33:40'
-),
-( 'peter', 'peter@chungu.co.ke', '81dc9bdb52d04dc20036dbd8313ed055', 'user', '2022-03-24 09:33:40', '2022-03-24 09:33:40'
+INSERT INTO users (
+    username,
+    email,
+    password,
+    role,
+    created_at,
+    updated_at
+  )
+VALUES(
+    'admin',
+    'admin@chungu.co.ke',
+    '81dc9bdb52d04dc20036dbd8313ed055',
+    'admin',
+    '2022-03-24 09:33:40',
+    '2022-03-24 09:33:40'
+  ),
+  (
+    'maeve',
+    'maeve@chungu.co.ke',
+    '81dc9bdb52d04dc20036dbd8313ed055',
+    'user',
+    '2022-03-24 09:33:40',
+    '2022-03-24 09:33:40'
+  ),
+  (
+    'peter',
+    'peter@chungu.co.ke',
+    '81dc9bdb52d04dc20036dbd8313ed055',
+    'user',
+    '2022-03-24 09:33:40',
+    '2022-03-24 09:33:40'
+  );
+CREATE TABLE `products` (
+  `id` varchar(300) PRIMARY KEY,
+  `name` varchar(300),
+  `price` varchar(300),
+  `quantity` varchar(300),
+  `image` varchar(300),
+  `category_id` varchar(300),
+  `created_at` datetime DEFAULT datetime('now','localtime') NOT NULL,
+  `updated_at` datetime DEFAULT datetime('now','localtime') NOT NULL
 );

8a74a1e415e633708c024edae466924aff1bf65e
munenepeter <peternjega7@gmail.com>
2022-05-31 13:18:00 +0300

    removed width contraint in the notification bar

--- Views/base.view.php
+++ Views/base.view.php
@@ -21,10 +21,10 @@ use Chungu\Core\Mantle\Request; ?>
 </head>
 
 <body class="">
- <?php $notification = implode(PHP_EOL, Request::$errors); ?>
+    <?php $notification = implode(PHP_EOL, Request::$errors); ?>
 
     <?php if (!empty($notification)) : ?>
-        <div class="fixed bottom-0 right-0 right-0 xl:right-20">
+        <div class="fixed bottom-0 right-0 right-0 ">
             <div id="toast-default" class="flex items-center w-full max-w-xs p-4 text-green-700 bg-green-100 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
                 <div class="ml-3 text-sm font-normal"><span><?= $notification; ?></span></div>
                 <button type="button" class="ml-4 -mx-1.5 -my-1.5 bg-pink-550 text-green-700 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">

2f20d5aeea573dfdf61b85411a6279ba8f008f40
Munene peter <peternjega7@gmail.com>
2022-06-02 20:04:43 +0300

    done with logs

--- Views/log.view.php
+++ Views/log.view.php
@@ -1,25 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include_once 'base.view.php';
+include_once 'sections/admin-nav.view.php'
+?>
+<link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.5.1/styles/default.min.css">
+<script src="https://unpkg.com/@highlightjs/cdn-assets@11.5.1/highlight.min.js"></script>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@11.5.1/styles/default.min.css">
-    <script src="https://unpkg.com/@highlightjs/cdn-assets@11.5.1/highlight.min.js"></script>
-    <!-- and it's easy to individually load additional languages -->
-    <title>Document</title>
-</head>
-
-<body>
-
-
-    <pre><code class="language-sql">
-  <?= $logs; ?>
+<div class="container px-4 mx-auto">
+<pre><code class="language-sql">  
+  <?= trim($logs) ?>
 </code></pre>
-    <script>
-        hljs.highlightAll();
-    </script>
+</div>
+<script>
+    hljs.highlightAll();
+</script>
 </body>
 
 </html>

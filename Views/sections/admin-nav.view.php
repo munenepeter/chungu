c4e65439313aa7686c807578dba700adb0de1012
Munene peter <peternjega7@gmail.com>
2022-06-02 20:15:31 +0300

    nav to bracelets

--- Views/sections/admin-nav.view.php
+++ Views/sections/admin-nav.view.php
@@ -42,7 +42,10 @@ use Chungu\Core\Mantle\Session;
                 </li>
 
                 <li>
-                    <a href="#" class="text-md block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-pink-550 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Bracelets</a>
+                 
+                    <a href="/-/addbracelets" class="text-md block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0
+                    <?= Request::uri() == '-/addbracelets' ? 'md:text-pink-550  md:hover:text-green-900' : 'hover:text-pink-550'; ?>
+                     md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Bracelets</a>
                 </li>
 
                 <li>

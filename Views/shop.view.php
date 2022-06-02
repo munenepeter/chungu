cd3ba8f3b89edb3509b10967c6c8b081b84d82f2
Munene peter <peternjega7@gmail.com>
2022-06-02 17:16:56 +0300

    scrollbar view

--- Views/shop.view.php
+++ Views/shop.view.php
@@ -5,41 +5,8 @@ include_once 'sections/nav.view.php'
 
 
 <main class="flex w-full">
-    <aside class="hidden md:block h-screen sticky top-4 -z-50 w-1/4 mt-16 p-6 sm:w-60 text-green-550">
-        <nav class="space-y-8 text-sm">
-            <div class="space-y-4">
-                <div class="flex flex-col space-y-4">
-                    <a class="text-lg hover:text-pink-550" rel="noopener noreferrer" href="#">New Arrivals
-                    </a>
-                    <a class="text-lg hover:text-pink-550" rel="noopener noreferrer" href="#">Best Sellers</a>
-                </div>
-            </div>
-            <div class="space-y-2">
-                <div class="flex flex-col space-y-2">
-                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Shop All
-                    </a>
-                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Earrings
-                    </a>
-                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Single Earrings
-                    </a>
-
-                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Rings
-                    </a>
-                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Necklaces + Pendants
-                    </a>
-                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Braceletes
-                    </a>
-                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Anklets
-                    </a>
-                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Wedding</a>
-                    <a class=" hover:text-pink-550" rel="noopener noreferrer" href="#">Men's</a>
-                </div>
-            </div>
-
-        </nav>
-    </aside>
-
-    <section class="md:h-screen md:overflow-y-auto body-font w-full">
+<?php include_once 'sections/side-nav.view.php';?>
+    <section id="shopItems" class="md:h-screen md:overflow-y-auto body-font w-full">
         <div class="container px-5 py-6 mx-auto">
             <center>
                 <a class="py-4" href="/shop/earrings">

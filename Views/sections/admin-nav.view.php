cc0b6631cee5f1556fe5439dc984794619031b27
Munene peter <peternjega7@gmail.com>
2022-06-02 20:07:20 +0300

    Added path to add anklets

--- Views/sections/admin-nav.view.php
+++ Views/sections/admin-nav.view.php
@@ -36,7 +36,9 @@ use Chungu\Core\Mantle\Session;
                      md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Necklaces</a>
                 </li>
                 <li>
-                    <a href="#" class="text-md block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-pink-550 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Anklets</a>
+                    <a href="/-/addanklets" class="text-md block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0
+                    <?= Request::uri() == '-/addanklets' ? 'md:text-pink-550  md:hover:text-green-900' : 'hover:text-pink-550'; ?>
+                     md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Anklets</a>
                 </li>
 
                 <li>

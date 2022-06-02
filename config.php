ddc0ca9a857280ed7ee2e7acbdc5871999a39aa6
Munene peter <peternjega7@gmail.com>
2022-06-02 12:19:18 +0300

    Added the db name

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
   //Get the configuration details ie DB connections
   //when using MYSQL
   'mysql' => [
-    'name' => 'clive',
+    'name' => 'chungu',
     'username' => '',
     'password' => '',
     'connection' => 'mysql:host=localhost',

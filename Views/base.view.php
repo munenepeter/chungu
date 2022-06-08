c1b00e40e5dbb41c27e5b1e5ea540573aa13721a
munenepeter <peternjega7@gmail.com>
2022-06-08 18:42:04 +0300

    works

--- Views/base.view.php
+++ Views/base.view.php
@@ -15,7 +15,7 @@ use Chungu\Core\Mantle\Request; ?>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cedarville+Cursive&display=swap" rel="stylesheet">
-     <script src="https://unpkg.com/flowbite@1.4.5/dist/flowbite.js"></script>
+    <script src="https://unpkg.com/flowbite@1.4.5/dist/flowbite.js"></script>
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" integrity="sha512-nIwdJlD5/vHj23CbO2iHCXtsqzdTTx3e3uAmpTm4x2Y8xCIFyWu4cSIV8GaGe2UNVq86/1h9EgUZy7tn243qdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Chungu <?= (Request::uri() == "") ? "" : "| " . ucwords(str_replace("/", " ", Request::uri())); ?></title>
@@ -32,16 +32,17 @@ use Chungu\Core\Mantle\Request; ?>
 
     ?>
 
-    <?php if (!empty($notification)) : ?>
-        <div class="fixed bottom-0 right-0 right-0 ">
-            <div id="toast-default" class="flex items-center w-full max-w-xs p-4 text-green-700 bg-green-100 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-                <div class="ml-3 text-sm font-normal"><span><?= $notification; ?></span></div>
-                <button type="button" class="ml-4 -mx-1.5 -my-1.5 bg-pink-550 text-green-700 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
-                    <span class="sr-only">Close</span>
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-            </div>
+
+    <div class="fixed bottom-0 right-0 right-0 ">
+        <div id="toast-default" class="flex items-center w-full max-w-xs p-4 text-green-700 bg-green-100 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
+            <div id="notif" class="ml-3 text-sm font-normal"><span><?= $notification; ?></span></div>
+            <button type="button" class="ml-4 -mx-1.5 -my-1.5 bg-pink-550 text-green-700 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
+                <span class="sr-only">Close</span>
+                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                </svg>
+            </button>
         </div>
+    </div>
+    <?php if (!empty($notification)) : ?>
     <?php endif; ?>
--- Views/signin.view.php
+++ Views/signin.view.php
@@ -7,14 +7,14 @@ include_once 'base.view.php';
         <div class="mt-28 w-full max-w-md p-8 space-y-3 rounded-xl bg-white border border-green-550">
             <h1 class="text-2xl font-bold text-center text-pink-550">Sign In</h1>
             <p class="bg-clip-text text-pink-550">Please fill in to continue to your account</p>
-            <form id="signin" novalidate="" action="" method="post" class="space-y-6 ng-untouched ng-pristine ng-valid">
+            <form id="signin" action="" method="post" class="space-y-6 ng-untouched ng-pristine ng-valid">
                 <div class="space-y-1 text-sm">
                     <label for="username" class="block text-left text-green-550">Username</label>
-                    <input type="text" name="username" id="username" placeholder="Username" class="w-full px-4 py-3 rounded-md border border-green-550 bg-gradient-to-r from-red-50 to-blue-50 text-green-550" required>
+                    <input type="text" name="username" id="username" placeholder="Username" class="w-full px-4 py-3 rounded-md border border-green-550 bg-gradient-to-r from-red-50 to-blue-50 text-green-550" required="">
                 </div>
                 <div class="space-y-1 text-sm">
                     <label for="password" class="block text-left text-green-550">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Password" class="w-full px-4 py-3 rounded-md border border-green-550 bg-gradient-to-r from-red-50 to-blue-50 text-green-550" required>
+                    <input type="password" name="password" id="password" placeholder="Password" class="w-full px-4 py-3 rounded-md border border-green-550 bg-gradient-to-r from-red-50 to-blue-50 text-green-550" required="">
                     <div class="flex justify-end text-xs text-blue-500 hover:text-blue-700">
                         <a rel="noopener noreferrer" href="#">Forgot Password?</a>
                     </div>
@@ -26,22 +26,26 @@ include_once 'base.view.php';
     </center>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script type="text/javascript">
-        var BASE_URL = "<?= url(); ?>";
+<script type="text/javascript">
+    var BASE_URL = "<?= url(); ?>";
 
-        $("#signin").submit(function(event) {
-            event.preventDefault();
-            $.ajax({
-                type: 'POST',
-                url: 'http://localhost:8989/signin',
-                data: $(this).serialize(),
-                success: function(data) {
+    $("#signin").submit(function(event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8989/signin',
+            data: $(this).serialize(),
+            success: function(data) {
+                if (typeof data === 'object' && data !== null) {
+                    data = JSON.stringify(JSON.parse(data));
+                } else {
                     data = JSON.parse(data);
-                    console.log(data);
                 }
-            });
+                $('#notif').text(data);
+            }
         });
-    </script>
+    });
+</script>
 </body>
 
 </html>

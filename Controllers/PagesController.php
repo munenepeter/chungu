b938f4059c2ca73c4f3e52e4605ff967c2fe5720
munenepeter <peternjega7@gmail.com>
2022-06-08 17:14:18 +0300

    test handler

--- Controllers/PagesController.php
+++ Controllers/PagesController.php
@@ -9,6 +9,9 @@ class PagesController extends Controller {
     public function index() {
         return view('index');
     }
+    public function test() {
+        return view('test');
+    }
 
     public function signin() {
         return view('signin');

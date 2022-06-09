6d2faf0a4a4a6f9792669e5da393cf339d2e5b42
munenepeter <peternjega7@gmail.com>
2022-06-09 17:17:21 +0300

    fixed delete bugs

--- Views/products.view.php
+++ Views/products.view.php
@@ -28,7 +28,7 @@ include_once 'sections/admin-nav.view.php';
                                  <?php  $category =  Category::find($product->category_id)[0];?>
                                 <tr class="group cursor-pointer hover:bg-green-50 border-b border-grey-light">
                                    <td class="text-sm p-3 whitespace-no-wrap"><?= ucwords($product->name); ?></td>
-                                   <td class="text-sm p-3 whitespace-no-wrap"><?= $category->name; ?></td>
+                                   <td class="text-sm p-3 whitespace-no-wrap"><?= ucwords($category->name); ?></td>
                                     <td class="hidden md:inline-flex text-sm p-3 whitespace-no-wrap">Ksh <?= $product->price; ?></td>
                                     <td class="text-sm p-3 whitespace-no-wrap text-center"><?= $product->quantity; ?></td>
                                     <td class="hidden md:inline-flex md:ml-28 text-sm p-3 whitespace-no-wrap text-center"><?= $product->status; ?></td>
@@ -132,7 +132,7 @@ include_once 'sections/admin-nav.view.php';
                                                     </div>
                                                 </div>
                                             </div>
-                                            <?php if (isAdmin()) : ?>
+                                         
                                                 <div x-data="{ open: false }">
                                                     <a @click.prevent="open = true" href="deleteuser?user_id<?= "$product->username" ?>">
                                                         <svg xmlns="http://www.w3.org/2000/svg" class="hover:text-red-600 text-red-400 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -149,16 +149,16 @@ include_once 'sections/admin-nav.view.php';
                                                                         </svg>
                                                                     </div>
                                                                     <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                                                        <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Delete <?= ucfirst($product->username); ?></h3>
+                                                                        <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Delete <?= ucfirst($product->name); ?></h3>
                                                                         <div class="mt-2">
-                                                                            <p class="text-sm text-gray-500">Are you sure you want to delete this user (<span class="font-medium "><?= ucfirst($product->username); ?></span>)? All of <span class="font-medium "><?= ucfirst("$product->username's"); ?></span> data will be permanently removed. This action cannot be undone!</p>
+                                                                            <p class="text-sm text-gray-500">Are you sure you want to delete this user (<span class="font-medium "><?= ucfirst($product->name); ?></span>)? All of <span class="font-medium "><?= ucfirst("$product->name's"); ?></span> data will be permanently removed. This action cannot be undone!</p>
                                                                         </div>
                                                                     </div>
 
                                                                 </div>
                                                                 <div class="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                                                                     <form action="users/delete" method="post">
-                                                                        <input type="hidden" name="id" value="<?= "$product->user_id"; ?>">
+                                                                        <input type="hidden" name="id" value="<?= "$product->id"; ?>">
                                                                         <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Delete</button>
                                                                     </form>
                                                                     <button @click="open = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-green-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
@@ -168,7 +168,7 @@ include_once 'sections/admin-nav.view.php';
                                                         </div>
                                                     </div>
                                                 </div>
-                                            <?php endif; ?>
+                                            
                                         </div>
                                     </td>
                                 </tr>

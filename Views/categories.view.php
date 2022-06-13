0b07120117c8cec9270db192c8c79698ce893064
munenepeter <peternjega7@gmail.com>
2022-06-13 13:16:23 +0300

    post action

--- Views/categories.view.php
+++ Views/categories.view.php
@@ -203,16 +203,16 @@ include_once 'sections/admin-nav.view.php'
                                                                     </svg>
                                                                 </div>
                                                                 <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                                                    <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Delete <?= ucfirst($category->category); ?></h3>
+                                                                    <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Delete <?= ucfirst($category->name); ?></h3>
                                                                     <div class="mt-2">
-                                                                        <p class="text-sm text-gray-500">Are you sure you want to delete this user (<span class="font-medium "><?= ucfirst($category->category); ?></span>)? All of <span class="font-medium "><?= ucfirst("$category->category's"); ?></span> data will be permanently removed. This action cannot be undone!</p>
+                                                                        <p class="text-sm text-gray-500">Are you sure you want to delete this category (<span class="font-medium "><?= ucfirst($category->name); ?></span>)? All of <span class="font-medium "><?= ucfirst("$category->name's"); ?></span> data will be permanently removed. This action cannot be undone!</p>
                                                                     </div>
                                                                 </div>
 
                                                             </div>
                                                             <div class="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                                                                <form action="users/delete" method="post">
-                                                                    <input type="hidden" name="id" value="<?= "$category->user_id"; ?>">
+                                                                <form action="categories/delete" method="post">
+                                                                    <input type="hidden" name="id" value="<?= "$category->id"; ?>">
                                                                     <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Delete</button>
                                                                 </form>
                                                                 <button @click="open = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-green-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>

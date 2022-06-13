8613d0cd91bcebde88da59dafe26b416923a06dd
munenepeter <peternjega7@gmail.com>
2022-06-13 15:25:13 +0300

    From the ui, one can delete a user

--- Views/users.view.php
+++ Views/users.view.php
@@ -69,7 +69,7 @@ include_once 'sections/admin-nav.view.php'
                                     <td class="text-sm p-3 whitespace-no-wrap text-sm group-hover:visible">
                                         <div class="md:text-base text-gray-800 flex items-center gap-2">
                                             <div x-data="{ open: false }">
-                                                <a @click.prevent="open = true" href="viewuser?user_id<?= "$user->user_id&uname=$user->username" ?>">
+                                                <a @click.prevent="open = true" href="viewuser?id<?= "$user->id&uname=$user->username" ?>">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="hover:text-blue-600 text-blue-400 w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
@@ -128,7 +128,7 @@ include_once 'sections/admin-nav.view.php'
 
 
                                             <div x-data="{ open: false }">
-                                                <a @click.prevent="open = true" href="edituser?user_id<?= $user->username; ?>">
+                                                <a @click.prevent="open = true" href="edituser?id<?= $user->username; ?>">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="hover:text-green-600 text-green-400 w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                                     </svg>
@@ -157,7 +157,7 @@ include_once 'sections/admin-nav.view.php'
                                                                         <option class="text-gray-900 text-sm rounded-lg" value="user">User</option> 
                                                                     </select> 
                                                             </div>
-                                                            <input type="hidden" name="id" value="<?= " $user->user_id"; ?>">
+                                                            <input type="hidden" name="id" value="<?= " $user->id"; ?>">
                                                             <div class="bg-green-50  sm:px-6 sm:flex sm:flex-row-reverse">
                                                                 <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm">Update</button>
                                                                 <button @click="open = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-green-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
@@ -171,7 +171,7 @@ include_once 'sections/admin-nav.view.php'
                                             </div>
                                             <?php if (isAdmin()) : ?>
                                                 <div x-data="{ open: false }">
-                                                    <a @click.prevent="open = true" href="deleteuser?user_id<?= "$user->username" ?>">
+                                                    <a @click.prevent="open = true" href="deleteuser?id<?= "$user->username" ?>">
                                                         <svg xmlns="http://www.w3.org/2000/svg" class="hover:text-red-600 text-red-400 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                                         </svg>
@@ -196,7 +196,7 @@ include_once 'sections/admin-nav.view.php'
                                                                 </div>
                                                                 <div class="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                                                                     <form action="users/delete" method="post">
-                                                                        <input type="hidden" name="id" value="<?= "$user->user_id"; ?>">
+                                                                        <input type="hidden" name="id" value="<?= "$user->id"; ?>">
                                                                         <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Delete</button>
                                                                     </form>
                                                                     <button @click="open = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-green-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>

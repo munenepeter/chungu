ad6a5583b25470dc5f362954531a0863d441ee2e
munenepeter <peternjega7@gmail.com>
2022-06-14 15:47:07 +0300

    done with product view

--- Views/product.view.php
+++ Views/product.view.php
@@ -11,48 +11,56 @@ $product = $product[0];
 <section class="text-green-550 body-font overflow-hidden">
     <div class="container px-5 py-8 mx-auto">
         <div class="lg:w-4/5 mx-auto flex flex-wrap">
-            <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400">
+            <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-96 h-64 object-cover object-center rounded" src="/../<?= $product->image; ?>">
             <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                <h2 class="text-sm title-font text-gray-500 tracking-widest">Earrings</h2>
-                <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
+                <div class="flex justify-between">
+                <h2 class="text-sm title-font text-green-550 tracking-widest"><?= ucwords($product->category_id); ?></h2>
+                <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-green-550 ml-4">
+                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5 text-white" viewBox="0 0 24 24">
+                            <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                        </svg>
+                    </button>
+                </div>
+                <h1 class="text-pink-550 text-3xl title-font font-medium mb-1"><?= ucwords($product->name); ?></h1>
                 <div class="flex mb-4">
+                    <!-- Reviews -->
                     <span class="flex items-center">
-                        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-pink-500" viewBox="0 0 24 24">
                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                         </svg>
-                        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-pink-500" viewBox="0 0 24 24">
                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                         </svg>
-                        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-pink-500" viewBox="0 0 24 24">
                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                         </svg>
-                        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-pink-500" viewBox="0 0 24 24">
                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                         </svg>
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-pink-500" viewBox="0 0 24 24">
                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                         </svg>
                         <span class="text-green-550 ml-3">4 Reviews</span>
                     </span>
                     <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                        <a class="text-gray-500">
+                        <a class="text-green-550">
                             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                             </svg>
                         </a>
-                        <a class="text-gray-500">
+                        <a class="text-green-550">
                             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                             </svg>
                         </a>
-                        <a class="text-gray-500">
+                        <a class="text-green-550">
                             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                                 <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                             </svg>
                         </a>
                     </span>
                 </div>
-                <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+                <p class="leading-relaxed">Beautiful earrings for every occassion</p>
                 <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                     <div class="flex">
                         <span class="mr-3">Color</span>
@@ -62,9 +70,9 @@ $product = $product[0];
                     </div>
                     <div class="flex ml-6 items-center">
                     <span class="mr-3">Quantity</span>
-                        <div class="ml-4 flex items-center" x-data="{ pax: 1 }">
+                        <div class="flex items-center" x-data="{ pax: 1 }">
                             <input type="button" value="-" class="font-semibold p-5" data-field="quantity" x-on:click="pax--;if(pax < 1){pax = 1;}">
-                            <input type="number" name="items" id="items" class="py-2 px-2 w-full border-gray-300 text-gray-900 text-sm rounded-lg" required min="1" max="4" :value="pax">
+                            <input type="number" name="quantity" id="quantity" class="text-center py-2 px-2 w-full border-gray-300 text-gray-900 text-sm rounded-lg" required min="1" max="4" :value="pax">
                             <input type="button" value="+" class="font-semibold p-5" data-field="quantity" x-on:click="pax++;if(pax > 5){pax = 1;}">
                         </div>
                         
@@ -73,83 +81,9 @@ $product = $product[0];
                 <div class="flex">
                     <span class="title-font font-medium text-2xl text-gray-900">Ksh <?= number_format($product->price, 2); ?></span>
                     <button style="background-color: #DE7B65;" class="flex ml-auto text-white border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Add to Bag</button>
-                    <button class="rounded-full w-10 h-10 bg-green-550 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                            <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                        </svg>
-                    </button>
+                   
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!-- <div class="grid place-items-center">
-   
-        <div class="w-full bg-white rounded-lg ">
-            <div class="flex justify-center">
-            <div class="img">
-            <img loading="lazy" class="h-64 object-cover transform transition duration-500 hover:scale-125 rounded-lg" src="/../<?= $product->image; ?>" alt="">
-            </div>
-            <div class="details">
-            <div class="mt-8">
-                <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400"><?= ucwords($product->name); ?></p>
-                <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Ksh <?= $product->price; ?></p>
-                <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Beautiful earrings for every occassion</p>
-                <h5 class="mb-4 text-xl font-semibold tracking-tight text-pink-550 dark:text-white">Available colors</h5>
-            </div>
-            <div class="items-center flex justify-start mb-2">
-                <fieldset class="items-center flex justify-start mb-2">
-                    <legend class="sr-only">Available Colors</legend>
-
-                    <div class="flex items-center">
-                        <input id="country-option-3" type="radio" name="color-gold" value="gold" class="w-6 h-6 bg-pink-550 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="country-option-3" class="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                            Gold
-                        </label>
-                    </div>
-                    <div class="flex items-center ml-4">
-                        <input id="country-option-3" type="radio" name="color-silver" value="silver" class="w-6 h-6 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="country-option-3" class="block ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                            Silver
-                        </label>
-                    </div>
-
-                    <div class="ml-4 flex items-center" x-data="{ pax: 1 }">
-                        <input type="button" value="-" class="font-semibold p-5" data-field="quantity" x-on:click="pax--;if(pax < 1){pax = 1;}">
-                        <input type="number" name="items" id="items" class="py-2 px-2 w-full border-gray-300 text-gray-900 text-sm rounded-lg" required min="1" max="4" :value="pax">
-                        <input type="button" value="+" class="font-semibold p-5" data-field="quantity" x-on:click="pax++;if(pax > 5){pax = 1;}">
-                    </div>
-                </fieldset>
-
-            </div>
-            <div class="flex justify-between">
-                <a href="/shop/earrings" class="lg:py-3 px-3  md:px-6 text-sm font-medium text-center text-pink-550">
-                    Return to Earrings
-                </a>
-                <button type="submit" style="background-color: #DE7B65;" class="lg:py-3 px-3  md:px-6 text-sm font-medium text-center text-white pink-550 rounded-lg hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800">
-                    Add to Bag
-                </button>
-            </div>
-        </div>
-        </div>
-    </div>
-
-    <a class="py-4" href="/shop/earrings">
-        <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">You May Also Like</h5>
-    </a>
-    <div class="flex space-x-6">
-        <?php for ($i = 0; $i < 4; $i++) : ?>
-            <div class="mx-auto p-4">
-                <div class="max-w-sm bg-white rounded-lg">
-                    <a class="" href="/shop/earrings?pro_id=8hcjd9">
-                        <img class="object-cover transform transition duration-500 hover:scale-125  w-48 h-64 rounded-lg " src="/../static/imgs/earrings/fancy-beads.jpg" alt="">
-                    </a>
-                    <div class="mt-8">
-                        <p style="font-family: 'Cedarville Cursive', cursive;" class="text-xl mb-2 font-extrabold text-gray-700 dark:text-gray-400">Chungu Collections</p>
-                        <p class="mb-4 font-semibold text-gray-700 dark:text-gray-400">Timeless darlings.</p>
-                    </div>
-                </div>
-            </div>
-        <?php endfor; ?>
-    </div>
-</div> -->

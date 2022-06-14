178d25765bfeec584c40a75e3b5d1417371bff7d
munenepeter <peternjega7@gmail.com>
2022-06-14 17:41:22 +0300

    fixed category id

--- Views/product.view.php
+++ Views/product.view.php
@@ -14,7 +14,7 @@ $product = $product[0];
             <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-96 h-64 object-cover object-center rounded" src="/../<?= $product->image; ?>">
             <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                 <div class="flex justify-between">
-                <h2 class="text-sm title-font text-green-550 tracking-widest"><?= ucwords($product->category_id); ?></h2>
+                <h2 class="text-sm title-font text-green-550 tracking-widest"><?= $product->category_id; ?></h2>
                 <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-green-550 ml-4">
                         <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5 text-white" viewBox="0 0 24 24">
                             <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
@@ -60,11 +60,10 @@ $product = $product[0];
                         </a>
                     </span>
                 </div>
-                <p class="leading-relaxed">Beautiful earrings for every occassion</p>
+                <p class="leading-relaxed">Beautiful <?= plural($product->category_id, 2); ?> for every occassion</p>
                 <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                     <div class="flex">
                         <span class="mr-3">Color</span>
-                        <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
                         <button class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
                         <button class="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
                     </div>

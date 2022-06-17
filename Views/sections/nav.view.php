cd2a28d7a676f4ab63163606541aa53675818acd
munenepeter <peternjega7@gmail.com>
2022-06-17 19:16:51 +0300

    Done for today

--- Views/sections/nav.view.php
+++ Views/sections/nav.view.php
@@ -37,21 +37,106 @@ use Chungu\Core\Mantle\Request;
                     <a href="#" class="text-md block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-pink-550 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Testimonials</a>
                 </li>
                 <li>
-                    <button class="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="text-green-550 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" class="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="text-green-550 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                         </svg>
-                        <?php if(!empty(session_get('cart_item')) || session_get('cart_item') !== null):?>
-                        <span class="absolute inset-0 object-right-top -mr-6">
-                            <div class="inline-flex items-center px-1 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
-                                <?=count(session_get('cart_item'));?>
-                            </div>
-                        </span>
-                        <?php endif;?>
+                        <?php if (!empty(session_get('cart_item')) || session_get('cart_item') !== null) : ?>
+                            <span class="hidden absolute inset-0 object-right-top -mr-6">
+                                <div id="cart-count" class="inline-flex items-center px-1 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
+
+                                    <?= count(session_get('cart_item')); ?>
+
+                                </div>
+                            </span>
+                        <?php endif; ?>
                     </button>
 
 
-                  
+
+                    <!-- Dropdown menu -->
+                    <div id="dropdownDivider" class="z-10 hidden bg-white right-10 rounded shadow w-96 dark:bg-gray-700 dark:divide-gray-600">
+                        <?php if (isset($_SESSION["cart_item"])) : ?>
+                            <?php $item_total = 0;
+
+                            echo '<table cellpadding="10" cellspacing="1">
+        <tbody>
+            <tr>
+                <th><strong>Name</strong></th>
+                <th><strong>Code</strong></th>
+                <th><strong>Quantity</strong></th>
+                <th><strong>Price</strong></th>
+                <th><strong>Action</strong></th>
+            </tr>';
+                            ?>
+                            <?php foreach ($_SESSION["cart_item"] as $item) : ?>
+
+
+                                <tr>
+                                    <td><strong><?= $item["name"]; ?></strong></td>
+                                    <td><?= $item["code"]; ?></td>
+                                    <td><?= $item["quantity"]; ?></td>
+                                    <td align=right><?= $item["price"]; ?></td>
+                                    <td><a onClick="cartAction('remove','<?= $item["code"]; ?>')" class="text-red-500">Remove Item</a></td>
+                                </tr>
+
+                                <?php $item_total += ($item["price"] * $item["quantity"]); ?>
+                            <?php endforeach; ?>
+                            <tr>
+                                <td colspan="5" align=right><strong>Total:</strong>Ksh<?= $item_total; ?></td>
+                            </tr>
+                            </tbody>
+                            </table>
+                        <?php endif; ?>
+
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+                        <script>
+                            function cartAction(action, product_code) {
+                                var queryString = "";
+                                if (action != "") {
+                                    switch (action) {
+                                        case "add":
+                                            queryString = 'action=' + action + '&code=' + product_code + '&quantity=' + $("#qty_" + product_code).val();
+                                            break;
+                                        case "remove":
+                                            queryString = 'action=' + action + '&code=' + product_code;
+                                            break;
+                                        case "empty":
+                                            queryString = 'action=' + action;
+                                            break;
+                                    }
+                                }
+                                jQuery.ajax({
+                                    url: '/shop',
+                                    data: queryString,
+                                    type: "POST",
+                                    success: function(data) {
+                                        $("#cart-item").html(data);
+                                        if (action != "") {
+                                            switch (action) {
+                                                case "add":
+                                                    $("#add_" + product_code).hide();
+                                                    $("#added_" + product_code).show();
+                                                    break;
+                                                case "remove":
+                                                    $("#add_" + product_code).show();
+                                                    $("#added_" + product_code).hide();
+                                                    break;
+                                                case "empty":
+                                                    $(".btnAddAction").show();
+                                                    $(".btnAdded").hide();
+                                                    break;
+                                            }
+                                        }
+                                    },
+                                    error: function() {}
+                                });
+                            }
+                        </script>
+                    </div>
+
+
                 </li>
 
                 <li>

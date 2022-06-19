e2beffdb1cc3556774fbb123dacd169f0fda20ff
Munene peter <peternjega7@gmail.com>
2022-06-19 12:59:37 +0300

    making the cart more like a cart

--- Views/sections/nav.view.php
+++ Views/sections/nav.view.php
@@ -37,7 +37,148 @@ use Chungu\Core\Mantle\Request;
                     <a href="#" class="text-md block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-pink-550 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Testimonials</a>
                 </li>
                 <li>
-                    <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" class="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
+
+                    <div x-data="{ open: false }">
+
+                        <button @click.prevent="open = true" class="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="text-green-550 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+                            </svg>
+                            <?php if (!empty(session_get('cart_item')) || session_get('cart_item') !== null) : ?>
+                                <span class="hidden absolute inset-0 object-right-top -mr-6">
+                                    <div id="cart-count" class="inline-flex items-center px-1 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
+
+                                        <?= count(session_get('cart_item')); ?>
+
+                                    </div>
+                                </span>
+                            <?php endif; ?>
+                        </button>
+
+                        <template x-if="open">
+                            <div class="mt-12" style="background-color: rgba(0,0,0,.5);">
+                               <div class="absolute  top-12 right-3  w-1/2 h-full">
+                                 <div class="text-left" @click.away="open = false">
+                                    <div class="border bg-green-50 p-2 my-2 rounded-lg">
+                                        <div class="rounded-lg bg-white w-full h-full border-b px-2 pb-6 my-2">
+                                            <div class="py-4">
+                                                <div>
+                                                    <h2 class="text-2xl font-semibold leading-tight">Shopping Bag</h2>
+                                                </div>
+                                                <div class="py-2 overflow-y-auto">
+                                                    <div class="inline-block min-w-full shadow-md rounded-lg overflow-y-auto">
+                                                        <table class="min-w-full leading-normal">
+                                                            <thead>
+                                                                <tr>
+                                                                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                                                        Client / Invoice
+                                                                    </th>
+                                                                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                                                        Amount
+                                                                    </th>
+                                                                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                                                        Issued / Due
+                                                                    </th>
+                                                                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                                                        Status
+                                                                    </th>
+                                                                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"></th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                        <div class="flex">
+                                                                            <div class="flex-shrink-0 w-10 h-10">
+                                                                                <img class="w-full h-full rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80" alt="" />
+                                                                            </div>
+                                                                            <div class="ml-3">
+                                                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                                                    Molly Sanders
+                                                                                </p>
+                                                                                <p class="text-gray-600 whitespace-no-wrap">000004</p>
+                                                                            </div>
+                                                                        </div>
+                                                                    </td>
+                                                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                        <p class="text-gray-900 whitespace-no-wrap">$20,000</p>
+                                                                        <p class="text-gray-600 whitespace-no-wrap">USD</p>
+                                                                    </td>
+                                                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                        <p class="text-gray-900 whitespace-no-wrap">Sept 28, 2019</p>
+                                                                        <p class="text-gray-600 whitespace-no-wrap">Due in 3 days</p>
+                                                                    </td>
+                                                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                        <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                                                            <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                                                            <span class="relative">Paid</span>
+                                                                        </span>
+                                                                    </td>
+                                                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
+                                                                        <button type="button" class="inline-block text-gray-500 hover:text-gray-700">
+                                                                            <svg class="inline-block h-6 w-6 fill-current" viewBox="0 0 24 24">
+                                                                                <path d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z" />
+                                                                            </svg>
+                                                                        </button>
+                                                                    </td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                        <div class="flex">
+                                                                            <div class="flex-shrink-0 w-10 h-10">
+                                                                                <img class="w-full h-full rounded-full" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80" alt="" />
+                                                                            </div>
+                                                                            <div class="ml-3">
+                                                                                <p class="text-gray-900 whitespace-no-wrap">
+                                                                                    Michael Roberts
+                                                                                </p>
+                                                                                <p class="text-gray-600 whitespace-no-wrap">000003</p>
+                                                                            </div>
+                                                                        </div>
+                                                                    </td>
+                                                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                        <p class="text-gray-900 whitespace-no-wrap">$214,000</p>
+                                                                        <p class="text-gray-600 whitespace-no-wrap">USD</p>
+                                                                    </td>
+                                                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                        <p class="text-gray-900 whitespace-no-wrap">Sept 25, 2019</p>
+                                                                        <p class="text-gray-600 whitespace-no-wrap">Due in 6 days</p>
+                                                                    </td>
+                                                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                                        <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                                                            <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                                                            <span class="relative">Paid</span>
+                                                                        </span>
+                                                                    </td>
+                                                                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
+                                                                        <button type="button" class="inline-block text-gray-500 hover:text-gray-700">
+                                                                            <svg class="inline-block h-6 w-6 fill-current" viewBox="0 0 24 24">
+                                                                                <path d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z" />
+                                                                            </svg>
+                                                                        </button>
+                                                                    </td>
+                                                                </tr>
+                                                                
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <div class="sm:px-6 sm:flex sm:flex-row-reverse">
+                                            <button @click="open = false" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-green-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </template>
+                    </div>
+
+
+
+                    <!-- <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" class="py-4 px-1 relative border-2 border-transparent text-gray-800 rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
                         <svg xmlns="http://www.w3.org/2000/svg" class="text-green-550 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                         </svg>
@@ -50,7 +191,8 @@ use Chungu\Core\Mantle\Request;
                                 </div>
                             </span>
                         <?php endif; ?>
-                    </button>
+                    </button> -->
+
 
 
 

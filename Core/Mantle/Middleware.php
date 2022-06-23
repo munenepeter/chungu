db4e3afd9915fe395e0e89034894c0cf8843ee70
munenepeter <peternjega7@gmail.com>
2022-06-23 10:53:16 +0300

    check middleware if is defined

--- Core/Mantle/Middleware.php
+++ Core/Mantle/Middleware.php
@@ -3,5 +3,14 @@
 namespace Chungu\Core\Mantle;
 
 class Middleware{
-    
+
+    protected $middleware = [
+        'auth'
+    ];
+
+    private function check_middleware($middleware){
+       if(!in_array($middleware, $this->middleware)){
+
+       }
+    }
 }

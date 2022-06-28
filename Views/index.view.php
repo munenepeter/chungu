cd8e1cdcafc96d58e25478f0c82c0153b601137f
munenepeter <peternjega7@gmail.com>
2022-06-28 12:58:49 +0300

    Move to index carousel

--- Views/index.view.php
+++ Views/index.view.php
@@ -6,39 +6,96 @@ include_once 'base.view.php';
 include_once 'sections/nav.view.php'
 ?>
 
-<section id="top-part">
-    <div class="mt-4 grid grid-cols-4 gap-4">
-        <div class="my-8">
-            <img src="../static/imgs/rotated-right.png" class="mt-72 h-20 md:h-28 lg:h-72 w-64" alt="" srcset="">
-        </div>
-        <div class="col-span-2 mx-auto">
-            <div class="max-w-sm bg-white rounded-lg">
+
+<div class="relative">
+        <!-- Carousel wrapper -->
+
+
+        <div class="mt-4 md:grid md:grid-cols-4 md:gap-4">
+            <div class="hidden md:block md:my-8">
+                <img src="../static/imgs/rotated-right.png"
+                    class="mt-72 h-20 md:h-28 lg:h-72 w-64" alt="" srcset="">
+            </div>
+            <div class="col-span-2 mx-auto">
                 <a href="/shop/offers">
-                    <h5 style="font-family: 'Cedarville Cursive', cursive;" class="text-center mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Father's Day offer</h5>
+                    <h5 style="font-family: 'Cedarville Cursive', cursive;"
+                        class="text-center mb-2 text-3xl font-black tracking-loose text-pink-550 dark:text-white">
+                        Father's Day offer</h5>
                 </a>
-                <a class="h-80" href="/shop/offers/6297963955ec6">
-                    <img class="transform transition duration-500 hover:scale-110 rounded-lg" src="<?php asset('imgs/offer/03.jpeg'); ?>" alt="">
-                </a>
-                <div class="mt-8">
-                    <p class="mb-8 font-semibold text-gray-700 dark:text-gray-400">Get our featured offer at 20% off this season.</p>
-                    <h5 class="mb-8 text-xl font-bold tracking-tight text-center text-pink-550 dark:text-white">Offer valid until 27th June 2022</h5>
+                <!-- 	<div class="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96"> -->
+                <div class="overflow-hidden relative h-80 px-2">
+
+                    <!-- Item 1 -->
+                    <div id="carousel-item-1" class="hidden duration-700 ease-in-out">
+                    <h2 class="text-green-550 text-center text-1xl font-black tracking-loose">Anklet</h2>
+                        <img src="<?php asset('imgs/offer/01.jpeg'); ?>"
+                            class="rounded-md block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
+                            alt="House">
+                    </div>
+                    <!-- Item 2 -->
+                
+                    <div id="carousel-item-2" class="hidden duration-700 ease-in-out">
+                    <h2 class="text-green-550 text-center text-1xl font-black tracking-loose">Necklace</h2>
+                        <img src="https://cdn.wallpapersafari.com/55/35/iy2arc.jpg"
+                            class="rounded-md block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
+                            alt="House">
+                    </div>
+                    <!-- Item 3 -->
+                    
+                    <div id="carousel-item-3" class="hidden duration-700 ease-in-out">
+                    <h2 class="text-green-550 text-center text-1xl font-black tracking-loose">Earring</h2>
+                        <img src="<?php asset('imgs/offer/03.jpeg'); ?>"
+                            class="rounded-md block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
+                            alt="House">
+                    </div>
+                    <!-- Item 4 -->
+                    <div id="carousel-item-4" class="hidden duration-700 ease-in-out">
+                        <h2 class="text-green-550 text-center text-1xl font-black tracking-loose">Belt</h2>
+                        <img src="https://cdn.wallpapersafari.com/8/46/3kSAXE.jpg"
+                            class="rounded-md block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
+                            alt="House">
+                    </div>
                 </div>
-                <div class="items-center flex justify-between space-x-2">
-                    <a href="#" class="py-2 lg:py-3 px-3  md:px-6 text-sm font-medium text-center text-white bg-green-550 rounded-lg hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800">
+                <!-- Slider indicators -->
+                <div class="my-2 flex justify-center space-x-3">
+                    <button id="carousel-indicator-1" type="button" class="w-3 h-3 rounded-full" aria-current="true"
+                        aria-label="Slide 1"></button>
+                    <button id="carousel-indicator-2" type="button" class="w-3 h-3 rounded-full" aria-current="false"
+                        aria-label="Slide 2"></button>
+                    <button id="carousel-indicator-3" type="button" class="w-3 h-3 rounded-full" aria-current="false"
+                        aria-label="Slide 3"></button>
+                    <button id="carousel-indicator-4" type="button" class="w-3 h-3 rounded-full" aria-current="false"
+                        aria-label="Slide 4"></button>
+                </div>
+
+                <div class="mt-8  text-center">
+                    <p class="mb-8 font-semibold text-gray-700 dark:text-gray-400">Get our featured offer at 20% off
+                        this season.</p>
+                    <h5 class="mb-8 text-xl font-bold tracking-tight text-pink-550 dark:text-white">Offer
+                        valid until 29th June 2022</h5>
+                </div>
+                <div class="items-center flex justify-between space-x-2 px-2">
+                    <a href="#"
+                        class="py-2 lg:py-3 px-3  md:px-6 text-sm font-medium text-center text-white bg-green-500 rounded-lg hover:bg-green-400 focus:ring-4 focus:outline-none focus:ring-green-300">
                         Add to Bag
                     </a>
-                    <a href="/offers" class="py-2 lg:py-3 px-3 md:px-6 text-sm font-medium text-center text-green-550 rounded-lg focus:ring-4 hover:bg-green-500 hover:text-white focus:outline-none focus:ring-green-300 border border-2 border-green-550">
+                    <a href="/offers"
+                        class="py-2 lg:py-3 px-3 md:px-6 text-sm font-medium text-center text-green-600 rounded-lg focus:ring-4 hover:bg-green-500 hover:text-white focus:outline-none focus:ring-green-300 border border-2 border-green-500">
                         More Offers
                     </a>
                 </div>
+
+            </div>
+            <div class="hidden md:block md:my-8">
+                <img style="float:right;" class="mt-72 h-20 md:h-28 lg:h-72 w-64"
+                    src="../static/imgs/olive.png"
+                    alt="" srcset="">
             </div>
         </div>
-        <div class="my-8">
-            <img style="float:right;" class="mt-72 h-20 md:h-28 lg:h-72 w-64" src="../static/imgs/olive.png" alt="" srcset="">
-        </div>
     </div>
-</section>
 
+ 
+ 
 <section id="second-part">
     <center>
         <h5 style="font-family: 'Cedarville Cursive'" class="mt-8 text-3xl font-black tracking-loose text-pink-550">Shop by Category</h5>

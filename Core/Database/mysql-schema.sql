fc4e31d2e7f8952820e6c0403730f5990c38dba1
munenepeter <peternjega7@gmail.com>
2022-06-29 11:12:46 +0300

    changed the schema to svg

--- Core/Database/mysql-schema.sql
+++ Core/Database/mysql-schema.sql
@@ -41,12 +41,13 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`,  `image`, `created_at`, `updated_at`) VALUES
-('cat-5ece4797eaf5e', 'earrings',  '/static/imgs/categories\\earrings.jpeg', '2022-05-31 10:16:47.000000', '2022-05-31 10:16:47.000000'),
-('cat-62962f89b29b3', 'rings',  '/static/imgs/categories\\rings.jpeg', '2022-06-01 10:16:47.000000', '2022-06-01 10:16:47.000000'),
-('cat-62962f98473f6', 'anklets',  '/static/imgs/categories\\anklets.jpeg', '2022-06-02 10:16:47.000000', '2022-06-02 10:16:47.000000'),
-('cat-62962fb1d0ca6', 'bracelets',  '/static/imgs/categories\\bracelets.jpeg', '2022-06-03 10:16:47.000000', '2022-06-03 10:16:47.000000'),
-('cat-62962ffec3fe7', 'belts',  '/static/imgs/categories\\belts.jpeg', '2022-06-03 10:16:47.000000', '2022-06-03 10:16:47.000000'),
-('cat-629785244dce4', 'necklaces',  '/static/imgs/categories\\necklaces.jpeg', '2022-06-03 10:16:47.000000', '2022-06-03 10:16:47.000000');
+('cat-5ece4797eaf5e', 'earrings',  '/static/imgs/categories\\earring.svg', '2022-06-29 10:16:47.000000', '2022-06-29 11:10:47.000000'),
+('cat-62962f89b29b3', 'rings',  '/static/imgs/categories\\ring.svg', '2022-06-11 10:16:47.000000', '2022-06-01 10:26:47.000000'),
+('cat-62962f98473f6', 'anklets',  '/static/imgs/categories\\anklet.svg', '2022-06-22 10:16:47.000000', '2022-06-27 10:16:47.000000'),
+('cat-62962fb1d0ca6', 'bracelets',  '/static/imgs/categories\\bracelet.svg', '2022-06-29 10:16:47.000000', '2022-06-24 10:16:47.000000'),
+('cat-62962ffec3fe7', 'belts',  '/static/imgs/categories\\belt.svg', '2022-06-20 10:16:47.000000', '2022-06-16 10:26:47.000000'),
+('cat-62ac4aec68b30', 'perfume',  '/static/imgs/categories\\perfume.svg', '2022-06-13 10:16:47.000000', '2022-06-26 10:16:47.000000'),
+('cat-629785244dce4', 'necklaces',  '/static/imgs/categories\\necklace.svg', '2022-06-23 10:16:47.000000', '2022-06-23 10:16:47.000000');
 
 -- --------------------------------------------------------
 

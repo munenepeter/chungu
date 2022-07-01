58f6a34ae318b85a9640394289674c4e45ae11e6
munenepeter <peternjega7@gmail.com>
2022-07-01 19:11:25 +0300

    Added some colors

--- Views/shop.view.php
+++ Views/shop.view.php
@@ -6,7 +6,7 @@ include_once 'sections/nav.view.php'
 
 <main class="flex w-full">
 <?php include_once 'sections/side-nav.view.php';?>
-    <section id="shopItems" class="md:h-screen md:overflow-y-auto body-font w-full">
+    <section id="shopItems" class="shadow-md rounded-md bg-gray-50 md:h-screen md:overflow-y-auto body-font w-full">
         <div class="container px-5 py-6 mx-auto">
             <center>
                 <a class="py-4" href="/shop/earrings">
@@ -15,7 +15,7 @@ include_once 'sections/nav.view.php'
             </center>
             <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 lg:gap-8 -m-4 sm:p-4">
                 <?php foreach($earrings as $earring) : ?>
-                    <div class=" p-4 w-full">
+                    <div class="bg-white p-4 w-full shadow-sm rounded-md">
                         <a href="shop/earrings/<?=$earring->id;?>" class="block relative h-64 rounded overflow-hidden">
                             <img loading="lazy" alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover  w-full h-full block" src="../<?=$earring->image;?>">
                         </a>
@@ -36,7 +36,7 @@ include_once 'sections/nav.view.php'
             </center>
             <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 lg:gap-8 -m-4 sm:p-4">
             <?php foreach($necklaces as $necklace) : ?>
-                    <div class=" p-4 w-full">
+                    <div class="bg-white p-4 w-full shadow-sm rounded-md">
                         <a href="shop/necklaces/<?=$necklace->id;?>" class="block relative h-64 rounded overflow-hidden">
                             <img loading="lazy" alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover  w-full h-full block" src="../<?=$earring->image;?>">
                         </a>
@@ -57,7 +57,7 @@ include_once 'sections/nav.view.php'
             </center>
             <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 lg:gap-8 -m-4 sm:p-4">
             <?php foreach($bracelets as $bracelet) : ?>
-                    <div class=" p-4 w-full">
+                    <div class="bg-white p-4 w-full shadow-sm rounded-md">
                         <a href="shop/bracelets/<?=$bracelet->id;?>" class="block relative h-64 rounded overflow-hidden">
                             <img loading="lazy" alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover  w-full h-full block" src="../<?=$earring->image;?>">
                         </a>
@@ -78,12 +78,12 @@ include_once 'sections/nav.view.php'
             </center>
             <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 lg:gap-8 -m-4 sm:p-4">
             <?php foreach($anklets as $anklet) : ?>
-                    <div class=" p-4 w-full">
+                    <div class="bg-white p-4 w-full shadow-sm rounded-md">
                         <a href="shop/anklets/<?=$anklet->id;?>" class="block relative h-64 rounded overflow-hidden">
                             <img loading="lazy" alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover  w-full h-full block" src="../<?=$earring->image;?>">
                         </a>
                         <div class="mt-4">
-                            <h3 class="text-pink-550 text-xs tracking-widest title-font mb-1">anklets</h3>
+                            <h3 class="text-pink-550 text-xs tracking-widest title-font mb-1">Anklets</h3>
                             <h2 style="font-family: 'Cedarville Cursive', cursive;" class="text-green-550 title-font text-lg font-medium"><?=ucwords($earring->name);?></h2>
                             <p class="mt-1 text-pink-550">Ksh<?=$anklet->price;?></p>
                         </div>

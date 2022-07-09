d5b2aa322169a0bf98ff5ea4a27e5fb00c3c3d6c
munenepeter <peternjega7@gmail.com>
2022-07-10 00:34:30 +0300

    using setUp fn

--- tests/Unit/RouterTest.php
+++ tests/Unit/RouterTest.php
@@ -7,32 +7,36 @@ use PHPUnit\Framework\TestCase;
 
 class RouterTest extends TestCase {
 
+    protected function setUp():void{
+
+        parent::setUp();
+
+        $this->router = new Router();
+        $this->routes_file = __DIR__."/../../routes.php";
+    }
+
     public function test_if_routes_file_exists(){
 
-        $routes_file = __DIR__."/../../routes.php";
-
-        $this->assertFileExists($routes_file);
+        $this->assertFileExists($this->routes_file);
     }
     
     public function test_it_creates_a_router_instance(){
-        $router = new Router();
 
-        $class = $router->load(__DIR__."/../../routes.php");
+        $class = $this->router->load($this->routes_file);
 
         $this->assertInstanceOf(Router::class, new Router);
     }
 
     public function test_it_registers_a_get_route(){
-        $router = new Router();
 
-        $router->load(__DIR__."/../../routes.php");
-        
-        $router->get('test', 'TestController@test');
+        $this->router->load($this->routes_file);
+
+        $this->router->get('test', 'TestController@test');
 
         $expected = [
             'test' => 'TestController@test'            
         ];
 
-        $this->assertEquals($expected, $router->routes['GET']);
+        $this->assertEquals($expected, $this->router->routes['GET']);
     }
 }

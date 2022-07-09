e2a3ee9e051e24d095a62425244ab584dcfed652
munenepeter <peternjega7@gmail.com>
2022-07-09 20:03:21 +0300

    Added diff color to headings

--- Views/privacy-policy.view.php
+++ Views/privacy-policy.view.php
@@ -4,78 +4,78 @@
 ?>
 
 <div class="bg-gray-100 p-5  border border-b">
-<h2>Chungu Ltd Privacy Policy</h2>
-<p>This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from (Store URL).</p>
+<p class="text-pink-550 text-lg py-4 font-black text-center">Chungu Ltd Privacy Policy</p>
+<p class="text-md">This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from <a href="https://chungu.co.ke">Chungu</a> .</p>
 
-<p>WHAT PERSONAL INFORMATION WE COLLECT</p>
+<p class="text-pink-550 text-lg py-4">WHAT PERSONAL INFORMATION WE COLLECT</p>
 
-<p>When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device.</p>
+<p class="text-md">When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device.</p>
 
-<p>Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically collected information as Device Information.</p>
+<p class="text-md">Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically collected information as Device Information.</p>
 
-<p>We collect Device Information using the following technologies:</p>
+<p class="text-md">We collect Device Information using the following technologies:</p>
 
-<p>Cookies are data files that are placed on your device or computer and often include an anonymous unique identifier.</p>
+<p class="text-md">Cookies are data files that are placed on your device or computer and often include an anonymous unique identifier.</p>
 
-<p>Log files track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.</p>
+<p class="text-md">Log files track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.</p>
 
-<p>Mention all other tracking tools and/or technologies being used by your website.</p>
+<p class="text-md">Mention all other tracking tools and/or technologies being used by your website.</p>
 
-<p>Also, when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including credit card numbers Mention all types of accepted payments, email address, and phone number. This is called Order Information.</p>
+<p class="text-md">Also, when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including credit card numbers Mention all types of accepted payments, email address, and phone number. This is called Order Information.</p>
 
-<p>Make sure you mention all other information that you collect.</p>
+<p class="text-md">Make sure you mention all other information that you collect.</p>
 
-<p>By Personal Information in this Privacy Policy, we are talking both about Device Information and Order Information.</p>
+<p class="text-md">By Personal Information in this Privacy Policy, we are talking both about Device Information and Order Information.</p>
 
-<p>HOW DO WE USE YOUR PERSONAL INFORMATION</p>
+<p class="text-pink-550 text-lg py-4">HOW DO WE USE YOUR PERSONAL INFORMATION</p>
 
-<p>We use the Order Information that we collect generally to fulfil any orders placed through the Site (including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations).</p>
+<p class="text-md">We use the Order Information that we collect generally to fulfil any orders placed through the Site (including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations).</p>
 
-<p>Additionally, we use this Order Information to:</p>
+<p class="text-md">Additionally, we use this Order Information to:</p>
 
-<p>Communicate with you.</p>
-<p>Screen our orders for potential risk or fraud.</p>
-<p>When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.</p>
-<p>We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site.</p>
+<p class="text-md">Communicate with you.</p>
+<p class="text-md">Screen our orders for potential risk or fraud.</p>
+<p class="text-md">When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.</p>
+<p class="text-md">We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site.</p>
 
-<p>SHARING YOUR PERSONAL INFORMATION</p>
+<p class="text-pink-550 text-lg py-4">SHARING YOUR PERSONAL INFORMATION</p>
 
-<p>We share your Personal Information with third parties to help us use your Personal Information, as described above.</p>
+<p class="text-md">We share your Personal Information with third parties to help us use your Personal Information, as described above.</p>
 
-<p>We also use Google Analytics to help us understand how our customers use (Store Name). How Google uses your Personal Information.</p>
+<p class="text-md">We also use Google Analytics to help us understand how our customers use Chungu. How Google uses your Personal Information.</p>
 
-<p>Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful requests for information we receive, or to otherwise protect our rights.</p>
+<p class="text-md">Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful requests for information we receive, or to otherwise protect our rights.</p>
 
-<p>BEHAVIOURAL ADVERTISING</p>
+<p class="text-pink-550 text-lg py-4">BEHAVIOURAL ADVERTISING</p>
 
-<p>We use your Personal Information to provide you with targeted advertisements or marketing communications we believe may be of interest to you.</p>
+<p class="text-md">We use your Personal Information to provide you with targeted advertisements or marketing communications we believe may be of interest to you.</p>
 
-<p>Mention opt-out links from external services such as:</p>
+<p class="text-md">Mention opt-out links from external services such as:</p>
 
-<p>Facebook</p>
-<p>Google</p>
-<p>You can opt out of targeted advertising…</p>
+<p class="text-md">Facebook</p>
+<p class="text-md">Google</p>
+<p class="text-md">You can opt out of targeted advertising…</p>
 
-<p>YOUR RIGHTS</p>
+<p class="text-pink-550 text-lg py-4">YOUR RIGHTS</p>
 
-<p>If you are a European resident, you have the right to access the personal information we hold about you and to ask that your personal information is corrected, updated, or deleted. If you would like to exercise this right, please contact us.</p>
+<p class="text-md">If you are a European resident, you have the right to access the personal information we hold about you and to ask that your personal information is corrected, updated, or deleted. If you would like to exercise this right, please contact us.</p>
 
-<p>Additionally, if you are a European resident we note that we are processing your information in order to fulfil contracts we might have with you (for example if you make an order through the Site), or otherwise to pursue our legitimate business interests listed above.</p>
-<p>Please note that your information will be transferred outside of Europe, including to Canada and the United States.</p>
+<p class="text-md">Additionally, if you are a European resident we note that we are processing your information in order to fulfil contracts we might have with you (for example if you make an order through the Site), or otherwise to pursue our legitimate business interests listed above.</p>
+<p class="text-md">Please note that your information will be transferred outside of Europe, including to Canada and the United States.</p>
 
-<p>DATA RETENTION</p>
+<p class="text-pink-550 text-lg py-4">DATA RETENTION</p>
 
-<p>When you place an order through the Site, we will maintain your Order Information for our records unless and until you ask us to delete this information.</p>
+<p class="text-md">When you place an order through the Site, we will maintain your Order Information for our records unless and until you ask us to delete this information.</p>
 
-<p>MINORS</p>
+<p class="text-pink-550 text-lg py-4">MINORS</p>
 
-<p>The Site is not intended for individuals under the age of (CLEARLY MENTION AGE).</p>
+<p class="text-md">The Site is not intended for individuals under the age of (CLEARLY MENTION AGE).</p>
 
-<p>CHANGES</p>
+<p class="text-pink-550 text-lg py-4">CHANGES</p>
 
-<p>We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.</p>
+<p class="text-md">We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.</p>
 
-<p>If you have questions and/or require more information, do not hesitate to contact us (Add Relevant contact information).</p>
+<p class="text-md">If you have questions and/or require more information, do not hesitate to contact us (Add Relevant contact information).</p>
 </div>
 <?php
  include_once 'sections/footer.view.php'

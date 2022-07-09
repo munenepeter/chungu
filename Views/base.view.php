1810a01c822990b58dd20ecbf6113aa2997cd74b
munenepeter <peternjega7@gmail.com>
2022-07-09 17:45:29 +0300

    tried minify

--- Views/base.view.php
+++ Views/base.view.php
@@ -1,9 +1,9 @@
 <?php
 
-use Chungu\Core\Mantle\Request;
-?>
+use Chungu\Core\Mantle\Request; ?>
 
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
@@ -13,7 +13,6 @@ use Chungu\Core\Mantle\Request;
     <link rel="icon" href="/../static/imgs/favicon/norm-favicon.svg" type="image/svg">
     <link rel="icon" href="<?php asset('imgs/favicon/norm-favicon.svg'); ?>" type="image/svg">
     <link rel="stylesheet" type="text/css" href="/../static/css/tailwind.css">
-    <!--<link rel="stylesheet" type="text/css" href="<?php asset('css/tailwind.css'); ?>">-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cedarville+Cursive&display=swap" rel="stylesheet">
@@ -21,39 +20,27 @@ use Chungu\Core\Mantle\Request;
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" integrity="sha512-nIwdJlD5/vHj23CbO2iHCXtsqzdTTx3e3uAmpTm4x2Y8xCIFyWu4cSIV8GaGe2UNVq86/1h9EgUZy7tn243qdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script defer src="/../static/js/index.js"></script>
-    <!--<script defer src="<?php asset('js/index.js'); ?>"></script>-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Chungu <?= (Request::uri() == "") ? "" : "| " . ucwords(str_replace("/", " - ", Request::uri())); ?></title>
-
-    <!-- Primary Meta Tags -->
     <meta name="title" content="Chungu — All your jewellery in one place">
     <meta name="description" content="Chungu has all your beloved jewels, from necklaces to rings, to belts and bracelets all that within Nairobi">
-
-    <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://chungu.co.ke/">
     <meta property="og:title" content="Chungu — All your jewellery in one place">
     <meta property="og:description" content="Chungu has all your beloved jewels, from necklaces to rings, to belts and bracelets all that within Nairobi">
     <meta property="og:image" content="<?php asset('imgs/offer/03.jpeg'); ?>">
-
-    <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://chungu.co.ke/">
     <meta property="twitter:title" content="Chungu — All your jewellery in one place">
     <meta property="twitter:description" content="Chungu has all your beloved jewels, from necklaces to rings, to belts and bracelets all that within Nairobi">
     <meta property="twitter:image" content="<?php asset('imgs/offer/03.jpeg'); ?>">
-
 </head>
-
-
 <script>
     function notify(text) {
-
         const notifyDiv = $('#notify');
         const notifyMsg = $('#notifyMsg');
-
         if (text === '') {
-            return
+            return;
         }
         notifyDiv.removeClass('hidden');
         notifyMsg.html(text);
@@ -64,12 +51,8 @@ use Chungu\Core\Mantle\Request;
 <body class="scroll-smooth">
     <div id="notify" class="hidden fixed bottom-0 right-0 right-0 ">
         <div id="toast-default" class="flex items-center w-full max-w-xs p-4 text-green-700 bg-green-100 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-            <div id="notifyMsg" class="ml-3 text-sm font-normal"></div>
-            <button type="button" class="ml-4 -mx-1.5 -my-1.5 bg-pink-550 text-green-700 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
-                <span class="sr-only">Close</span>
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <div id="notifyMsg" class="ml-3 text-sm font-normal"></div><button type="button" class="ml-4 -mx-1.5 -my-1.5 bg-pink-550 text-green-700 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close"> <span class="sr-only">Close</span> <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                </svg>
-            </button>
+                </svg> </button>
         </div>
     </div>

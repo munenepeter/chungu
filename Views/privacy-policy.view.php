f00f94673ee2bbc7b9831d13a098b3ca54479327
munenepeter <peternjega7@gmail.com>
2022-07-09 20:11:14 +0300

    Done with privacy policy

--- Views/privacy-policy.view.php
+++ Views/privacy-policy.view.php
@@ -5,7 +5,7 @@
 
 <div class="bg-gray-100 p-5  border border-b">
 <p class="text-pink-550 text-lg py-4 font-black text-center">Chungu Ltd Privacy Policy</p>
-<p class="text-md">This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from <a href="https://chungu.co.ke">Chungu</a> .</p>
+<p class="text-md">This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from <a class="text-teal-600 hover:underline" href="https://chungu.co.ke">Chungu</a> .</p>
 
 <p class="text-pink-550 text-lg py-4">WHAT PERSONAL INFORMATION WE COLLECT</p>
 
@@ -19,11 +19,9 @@
 
 <p class="text-md">Log files track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.</p>
 
-<p class="text-md">Mention all other tracking tools and/or technologies being used by your website.</p>
 
-<p class="text-md">Also, when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including credit card numbers Mention all types of accepted payments, email address, and phone number. This is called Order Information.</p>
+<p class="text-md">Also, when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including *credit card numbers Mention all types of accepted payments, email address, and phone number. This is called Order Information.</p>
 
-<p class="text-md">Make sure you mention all other information that you collect.</p>
 
 <p class="text-md">By Personal Information in this Privacy Policy, we are talking both about Device Information and Order Information.</p>
 
@@ -42,7 +40,7 @@
 
 <p class="text-md">We share your Personal Information with third parties to help us use your Personal Information, as described above.</p>
 
-<p class="text-md">We also use Google Analytics to help us understand how our customers use Chungu. How Google uses your Personal Information.</p>
+<p class="text-md">We also use Google Analytics to help us understand how our customers use Chungu. <a class="text-teal-600 hover:underline" href="https://policies.google.com/technologies/partner-sites?hl=en-US#:~:text=Google%20uses%20the%20information%20shared,our%20partners'%20sites%20and%20apps." target="_blank" rel="noopener noreferrer">How Google uses your Personal Information.</a></p>
 
 <p class="text-md">Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful requests for information we receive, or to otherwise protect our rights.</p>
 
@@ -50,17 +48,12 @@
 
 <p class="text-md">We use your Personal Information to provide you with targeted advertisements or marketing communications we believe may be of interest to you.</p>
 
-<p class="text-md">Mention opt-out links from external services such as:</p>
-
-<p class="text-md">Facebook</p>
-<p class="text-md">Google</p>
-<p class="text-md">You can opt out of targeted advertising…</p>
 
 <p class="text-pink-550 text-lg py-4">YOUR RIGHTS</p>
 
-<p class="text-md">If you are a European resident, you have the right to access the personal information we hold about you and to ask that your personal information is corrected, updated, or deleted. If you would like to exercise this right, please contact us.</p>
+<p class="text-md">You have the right to access the personal information we hold about you and to ask that your personal information is corrected, updated, or deleted. If you would like to exercise this right, please contact us.</p>
 
-<p class="text-md">Additionally, if you are a European resident we note that we are processing your information in order to fulfil contracts we might have with you (for example if you make an order through the Site), or otherwise to pursue our legitimate business interests listed above.</p>
+<p class="text-md">Additionally, we note that we are processing your information in order to fulfil contracts we might have with you (for example if you make an order through the Site), or otherwise to pursue our legitimate business interests listed above.</p>
 <p class="text-md">Please note that your information will be transferred outside of Europe, including to Canada and the United States.</p>
 
 <p class="text-pink-550 text-lg py-4">DATA RETENTION</p>
@@ -69,13 +62,13 @@
 
 <p class="text-pink-550 text-lg py-4">MINORS</p>
 
-<p class="text-md">The Site is not intended for individuals under the age of (CLEARLY MENTION AGE).</p>
+<p class="text-md">The Site is not intended for individuals under the age of (6 Years.</p>
 
 <p class="text-pink-550 text-lg py-4">CHANGES</p>
 
 <p class="text-md">We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.</p>
 
-<p class="text-md">If you have questions and/or require more information, do not hesitate to contact us (Add Relevant contact information).</p>
+<p class="text-md">If you have questions and/or require more information, do not hesitate to contact us through <a class="text-teal-600 hover:underline" href="mailto:info@chungu.co.ke">info@chungu.co.ke</a>.</p>
 </div>
 <?php
  include_once 'sections/footer.view.php'

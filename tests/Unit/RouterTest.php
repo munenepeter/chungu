ef021f6bd7eb48cee6310129f0f18fc5eb19fe32
munenepeter <peternjega7@gmail.com>
2022-07-10 00:19:23 +0300

    test if config exists, test if load works

--- tests/Unit/RouterTest.php
+++ tests/Unit/RouterTest.php
@@ -2,9 +2,23 @@
 
 namespace Tests\Unit;
 
+use Chungu\Core\Mantle\Router;
 use PHPUnit\Framework\TestCase;
 
 class RouterTest extends TestCase {
 
+    public function test_if_config_file_exists(){
+
+        $config_file = __DIR__."/../../config.php";
+
+        $this->assertFileExists($config_file);
+    }
     
+    public function test_it_creates_a_router_instance(){
+        $router = new Router();
+
+        $class = $router->load(__DIR__."/../../config.php");
+
+        $this->assertInstanceOf(Router::class, new Router);
+    }
 }

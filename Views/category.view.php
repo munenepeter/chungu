06280adaab6ce30f7b9587f3da750aac5f8636a5
munenepeter <peternjega7@gmail.com>
2022-07-19 13:33:32 +0300

    Added wishlist btn

--- Views/category.view.php
+++ Views/category.view.php
@@ -8,7 +8,7 @@ include_once 'sections/nav.view.php';
 <main class="flex w-full">
     <?php
     include_once 'sections/side-nav.view.php';
-    
+
     ?>
 
     <section id="" class="md:h-screen md:overflow-y-auto w-full md:overflow-x-hidden border-b">
@@ -17,22 +17,31 @@ include_once 'sections/nav.view.php';
                 <a class="py-4" href="/shop/<?= $category_name ?>">
                     <h5 style="font-family: 'Cedarville Cursive', cursive;" class="mb-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white"><?= ucfirst($category_name); ?></h5>
                 </a>
-               
-            </center> 
+
+            </center>
             <?php if (empty($products)) : ?>
-                <h5 style="font-family: cursive;" class="text-center mt-22 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Oops, Seems there are no <?=$category_name?> yet <br> Please come back later!</h5>
+                <h5 style="font-family: cursive;" class="text-center mt-22 text-3xl font-black tracking-loose text-pink-550 dark:text-white">Oops, Seems there are no <?= $category_name ?> yet <br> Please come back later!</h5>
 
             <?php else : ?>
                 <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 lg:gap-8 -m-4 sm:p-4">
                     <?php foreach ($products as $product) : ?>
                         <div class="p-4 w-full">
-                            <a href="<?=$category_name?>/<?= $product->id; ?>" class="block relative xl:h-72 h-56 rounded overflow-hidden">
-                                <img loading="lazy" alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover w-full h-full block" src="../<?= $product->image; ?>">
-                            </a>
+                            <div class="border p-2 shadow-sm bg-gray-50 rounded-md">
+                                <a href="<?= $category_name ?>/<?= $product->id; ?>" class="block relative xl:h-72 h-56 rounded overflow-hidden">
+                                    <img loading="lazy" alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover w-full h-full block" src="../<?= $product->image; ?>">
+                                </a>
+                            </div>
                             <div class="mt-4">
-                                <h3 class="text-pink-550 text-xs tracking-widest title-font mb-1"><?= strtoupper($category_name); ?></h3>
+                                <div class="flex justify-between items-center">
+                                    <h3 class="text-pink-550 text-xs tracking-widest title-font mb-1"><?= strtoupper($category_name); ?></h3>
+                                    
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="text-pink-550 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                    </svg>
+                                     
+                                </div>
                                 <h2 style="font-family: 'Cedarville Cursive', cursive;" class="text-green-550 title-font text-lg font-medium"><?= $product->name; ?></h2>
-                                <p class="mt-1 text-pink-550">Ksh <?= number_format($product->price, 2); ?></p>
+                                <p class="mt-1 text-pink-550">Ksh  <?= number_format($product->price, 2); ?></p>
                             </div>
                         </div>
                     <?php endforeach; ?>

f2ffce5cf20fb43534b0ad77b7eae9b83d4749fa
munenepeter <peternjega7@gmail.com>
2022-07-26 13:41:54 +0300

    Gettin ready with sales

--- Views/sales.view.php
+++ Views/sales.view.php
@@ -1,2 +1,10 @@
-<?php include_once 'base.view.php';?><div class="grid place-items-center h-screen">
-       Created Views/sales.view.php's view; please edit</div>
+<?php
+
+include_once  'base.view.php';
+include_once 'sections/admin-nav.view.php';
+
+?>
+<div class="grid place-items-center">
+
+
+</div>

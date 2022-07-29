bd1d9d1edcde83933254a910a255e8bbdaa4fa07
munenepeter <peternjega7@gmail.com>
2022-07-29 15:53:43 +0300

    add full height in body

--- Views/base.view.php
+++ Views/base.view.php
@@ -54,7 +54,7 @@ use Chungu\Core\Mantle\Request; ?>
     }
 </script>
 
-<body class="scroll-smooth">
+<body class="h-screen scroll-smooth">
     <div id="notify" class="hidden fixed bottom-0 right-0 right-0">
         <div id="toast-default" class="flex items-center w-full max-w-xs p-4 text-green-700 bg-green-100 rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
             <div id="notifyMsg" class="ml-3 text-sm font-normal"></div><button type="button" class="ml-4 -mx-1.5 -my-1.5 bg-pink-550 text-green-700 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close"><span class="sr-only">Close</span><svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">

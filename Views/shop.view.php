640d766ed47f7634fea47a9f7ad187d38fed586f
munenepeter <peternjega7@gmail.com>
2022-08-04 16:00:11 +0300

    complete overhaul of the signin

--- Views/shop.view.php
+++ Views/shop.view.php
@@ -6,13 +6,32 @@ include_once 'sections/nav.view.php';
 
 <main class="flex w-full shadow-inner">
     <section id="shopItems" class="shadow-md rounded-md bg-gray-50 md:h-fit md:overflow-y-auto body-font w-full">
+
+
+
         <?php if (empty($earrings)) : ?>
-         
+
         <?php else : ?>
             <div class="container px-5 py-6 mx-auto">
+
+
+
+                <div class="grid grid-cols-3 gap-4 p-5">
+                    <div class="shadow-lg bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg row-span-2">1</div>
+                    <div class="shadow-lg bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">2</div>
+                    <div class="shadow-lg bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg row-span-2">3</div>
+                    <div class="shadow-lg bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg">4</div>
+                    <div class="shadow-lg bg-green-100 text-green-500 text-lg font-bold text-center p-10 rounded-lg col-span-3">5</div>
+                    
+                </div>
+
+
+
+
+
                 <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 lg:gap-8 -m-4 sm:p-4">
                     <?php foreach ($earrings as $earring) : ?>
-                        <div class="bg-white p-4 w-full shadow-sm rounded-md">
+                        <!-- <div class="bg-white p-4 w-full shadow-sm rounded-md">
                             <a href="shop/earrings/<?= $earring->id; ?>" class="block relative h-64 rounded overflow-hidden">
                                 <img loading="lazy" alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover  w-full h-full block" src="../<?= $earring->image; ?>">
                             </a>
@@ -21,7 +40,7 @@ include_once 'sections/nav.view.php';
                                 <h2 style="font-family: 'Cedarville Cursive', cursive;" class="text-green-550 title-font text-lg font-medium"><?= ucwords($earring->name); ?></h2>
                                 <p class="mt-1 text-pink-550">Ksh<?= $earring->price; ?></p>
                             </div>
-                        </div>
+                        </div> -->
                     <?php endforeach; ?>
                 </div>
             </div>

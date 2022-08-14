bd15ab257672073ee37240fafe9d140990c9c520
Peter Munene <peternjega7@gmail.com>
2022-08-14 18:06:04 +0300

    release pages contoller

--- Controllers/PagesController.php
+++ Controllers/PagesController.php
@@ -3,8 +3,6 @@
 namespace Chungu\Controllers;
 
 use Chungu\Models\Category;
-use Chungu\Models\Source;
-use Chungu\Models\User;
 
 class PagesController extends Controller {
 
@@ -32,42 +30,6 @@ class PagesController extends Controller {
         ]);
     }
 
- 
-    public function sources() {
-        //check if the user is logged in
-        $this->middleware('auth');
-        
-        return view('sources',[
-            'sources' => Source::all()
-        ]);
-    }
-    public function store_source()
-    {
-        $this->middleware('auth');  
-        //validate the input
-        $this->request()->validate($_POST, [
-            'name' => 'required',
-            'email' => 'required',
-            'location' => 'required',
-            'link' => 'required',
-            'phone' => 'required',
-        ]);
-        //create user
-        Source::create([
-            'name' => $this->request()->form('name'),
-            'email' => $this->request()->form('email'),
-            'location' => $this->request()->form('location'),
-            'link' => $this->request()->form('link'),
-            'phone' => $this->request()->form('phone'), 
-            'created_at' => date('Y-m-d H:i:s', time()),
-            'updated_at' => date('Y-m-d H:i:s', time())
-        ]);
-        //notify    
-        notify("New Source added");
-
-        //redirect back
-        return redirectback();
-    }
     public function privacy() {
         return view('privacy-policy');
     }

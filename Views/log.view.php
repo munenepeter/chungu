4151200a7887950c89b81d4f344d02db3720f256
munenepeter <peternjega7@gmail.com>
2022-08-27 02:41:02 +0300

    done

--- Views/log.view.php
+++ Views/log.view.php
@@ -1,6 +1,8 @@
 <?php
 include_once 'base.view.php';
-include_once 'sections/admin-nav.view.php'
+include_once 'sections/admin-nav.view.php';
+
+$all = implode(",", $logs);
 ?>
 
 <div class="bg-gray-100 m-4 rounded-md">
@@ -11,15 +13,16 @@ include_once 'sections/admin-nav.view.php'
         <div class="w-full flex items-center py-2">
             <div class="hidden md:flex items-center">
                 <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Debug
-                    421</button>
+                    <?= substr_count($all, 'Debug') ?></button>
                 <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Info
-                    2,162</button>
+                    <?= substr_count($all, 'Info') ?></button>
                 <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Errors
-                    651</button>
+                    <?= substr_count($all, 'Error') ?></button>
                 <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:focus:ring-yellow-900">Warnings
-                    9,852</button>
+                    <?= substr_count($all, 'Warning') ?></button>
             </div>
-            <form class="ml-auto">
+            <div class="ml-auto flex items-center">
+            <form class="mr-8">
                 <label for="simple-search" class="sr-only">Search</label>
                 <div class="relative w-full">
                     <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
@@ -30,6 +33,10 @@ include_once 'sections/admin-nav.view.php'
                     <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search ..." required="">
                 </div>
             </form>
+
+            <button type="button" class="focus:outline-none text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:focus:ring-red-900">Delete Logs</button>
+            </div>
+            
 
         </div>
 
@@ -54,9 +61,7 @@ include_once 'sections/admin-nav.view.php'
                 </thead>
                 <tbody class="">
                     <?php foreach ($logs as $log) : ?>
-                        <?php $log = json_decode($log);
-
-                        ?>
+                        <?php $log = json_decode($log); ?>
                         <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                             <th scope="row" class="py-4 px-2 md:px-6 flex items-center">
                                 <?php if ($log->level === "Error") : ?>

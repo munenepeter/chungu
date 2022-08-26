60d80d8b92633c687f1eebaeff97d865e5f612b9
munenepeter <peternjega7@gmail.com>
2022-08-26 19:17:23 +0300

    refactor logger

--- Core/Mantle/Logger.php
+++ Core/Mantle/Logger.php
@@ -2,11 +2,13 @@
 
 namespace Chungu\Core\Mantle;
 
+use Chungu\Core\Mantle\Request;
+
 class Logger {
     
     public static function log(String $msg){
   
-        $log = date("D, d M Y H:i:s") . ' - ' . $_SERVER['SERVER_NAME'] . ' - ' . $_SERVER['REMOTE_ADDR'] . ' - ' . "$msg" . PHP_EOL;
+        $log = date("D, d M Y H:i:s") . ' - ' .  Request::method()  . ' - /' . Request::uri() . ' - ' . $_SERVER['REMOTE_ADDR'] . ' - ' . "$msg" . PHP_EOL;
   
         $logFile =  __DIR__."/Logs/logs.log";
 

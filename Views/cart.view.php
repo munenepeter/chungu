780bdc6fff080540d9dab0ed65a1a7db47bc3750
munenepeter <peternjega7@gmail.com>
2022-08-26 19:01:27 +0300

    working with php

--- Views/cart.view.php
+++ Views/cart.view.php
@@ -16,6 +16,8 @@ include_once 'sections/nav.view.php';
     </div>
 
     <div x-data="{ open: true }" class="fixed inset-0 overflow-hidden">
+    <?php if (isset($_SESSION["cart_item"])) : ?>
+        <?php $item_total = 0; ?>
         <template x-if="open">
             <div class="absolute inset-0 overflow-hidden">
                 <div class="pointer-events-none fixed inset-y-0 top-20 right-0 flex max-w-full pl-10">
@@ -37,55 +39,36 @@ include_once 'sections/nav.view.php';
                                 <div class="mt-8">
                                     <div class="flow-root">
                                         <ul role="list" class="-my-6 divide-y divide-gray-200">
-                                            <li class="flex py-6">
+
+                                        <?php foreach ($_SESSION["cart_item"] as $item) : ?>
+
+                                            <li id="row_<?= $item["id"]; ?>" class="flex py-6">
                                                 <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                                    <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." class="h-full w-full object-cover object-center">
+                                                    <img src="<?php asset("../../" . $item["image"]); ?>" alt="" class="h-full w-full object-cover object-center">
                                                 </div>
 
                                                 <div class="ml-4 flex flex-1 flex-col">
                                                     <div>
                                                         <div class="flex justify-between text-base font-medium text-gray-900">
                                                             <h3>
-                                                                <a href="#"> Throwback Hip Bag </a>
+                                                                <a href="#"><?= $item["name"]; ?> </a>
                                                             </h3>
-                                                            <p class="ml-4">$90.00</p>
+                                                            <p class="ml-4">Ksh<?= $item["price"];?>.00</p>
                                                         </div>
                                                         <p class="mt-1 text-sm text-gray-500">Salmon</p>
                                                     </div>
                                                     <div class="flex flex-1 items-end justify-between text-sm">
-                                                        <p class="text-gray-500">Qty 1</p>
+                                                        <p class="text-gray-500">Qty <?= $item["quantity"]; ?></p>
 
                                                         <div class="flex">
-                                                            <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                                                            <button onClick="cartAction('remove','<?= $item["id"]; ?>')" type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </li>
-
-                                            <li class="flex py-6">
-                                                <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                                    <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg" alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch." class="h-full w-full object-cover object-center">
-                                                </div>
-
-                                                <div class="ml-4 flex flex-1 flex-col">
-                                                    <div>
-                                                        <div class="flex justify-between text-base font-medium text-gray-900">
-                                                            <h3>
-                                                                <a href="#"> Medium Stuff Satchel </a>
-                                                            </h3>
-                                                            <p class="ml-4">$32.00</p>
-                                                        </div>
-                                                        <p class="mt-1 text-sm text-gray-500">Blue</p>
-                                                    </div>
-                                                    <div class="flex flex-1 items-end justify-between text-sm">
-                                                        <p class="text-gray-500">Qty 1</p>
-
-                                                        <div class="flex">
-                                                            <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </li>
+                                            <?php $item_total += ($item["price"] * $item["quantity"]); ?>
+                                    <?php endforeach; ?>
+                                         
 
                                             <!-- More products... -->
                                         </ul>
@@ -96,7 +79,7 @@ include_once 'sections/nav.view.php';
                             <div class="border-t border-gray-200 py-6 px-4 sm:px-6">
                                 <div class="flex justify-between text-base font-medium text-gray-900">
                                     <p>Subtotal</p>
-                                    <p>$262.00</p>
+                                    <p>Ksh<?= $item_total; ?>.00</p>
                                 </div>
                                 <p class="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
                                 <div class="mt-6">
@@ -113,6 +96,7 @@ include_once 'sections/nav.view.php';
                 </div>
             </div>
         </template>
+        <?php endif; ?>
     </div>
 </div>
 

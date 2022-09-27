0e54dc77ecbb805c4131451c300c408b8e1aa211
munenepeter <peternjega7@gmail.com>
2022-09-27 18:53:21 +0300

    Add more info

--- Core/Mantle/Logger.php
+++ Core/Mantle/Logger.php
@@ -7,19 +7,24 @@ use Chungu\Core\Mantle\Request;
 class Logger {
 
     public static function log(String $level, String $msg) {
+        $userinfo = json_decode(file_get_contents('http://ip-api.io/json/' . $_SERVER['REMOTE_ADDR']));
 
-        // $log = date("D, d M Y H:i:s") . ' - ' .  Request::method()  . ' - /' . Request::uri() . ' - ' . $_SERVER['REMOTE_ADDR'] . ' - ' . "$msg" . PHP_EOL;
-        $log = json_encode( [
+        $log = json_encode([
             'level' => $level,
             'time' => date("D, d M Y H:i:s"),
             "more" => [
                 "method" => Request::method(),
                 "uri" => '/' . Request::uri(),
                 "remote_addr" => $_SERVER['REMOTE_ADDR'],
+                "region" => $userinfo->region_name,
+                "country" => $userinfo->country_name,
+                "city" => $userinfo->city,
+                "provider" => $userinfo->organisation,
+                "time_zone" => $userinfo->time_zone,
                 "agent" => $_SERVER['HTTP_USER_AGENT']
             ],
             "desc" => nl2br($msg)
-        ]). PHP_EOL;
+        ]) . PHP_EOL;
 
         $logFile =  __DIR__ . "/Logs/logs.log";
 

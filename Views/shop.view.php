0f37e36c41baebcae342f3ef03ded20c0bbf0b75
munenepeter <peternjega7@gmail.com>
2022-09-30 17:17:24 +0300

    Added new ccs & overlay now works

--- Views/shop.view.php
+++ Views/shop.view.php
@@ -2,126 +2,41 @@
 include_once 'base.view.php';
 include_once 'sections/nav.view.php';
 ?>
+<style>
 
+</style>
 
+<link rel="stylesheet" href="../static/css/shop.css">
 <main class="flex w-full shadow-inner">
     <section id="shopItems" class="shadow-md rounded-md bg-gray-50 md:h-fit md:overflow-y-auto body-font w-full">
-
-
-
         <?php if (empty($categories)) : ?>
 
         <?php else : ?>
-            <div class="container px-5 py-6 mx-auto">
-
-                <?php
-                //dd(reset($categories)->image)
-                ?>
-
-                <div class="flex flex-col md:grid md:grid-cols-3 md:gap-4 p-5 space-y-4">
-                    <div class="shadow-lg bg-green-50 hover:bg-green-100 text-pink-550 text-lg font-bold text-center rounded-lg row-span-2">
-                        <?php
-                        $category = array_shift($categories);
-                        ?>
-                        <div class="">
-                            <h1 class="pt-6 text-3xl font-thin"><?= strtoupper($category->name); ?></h1>
-                            <div class="relative h-48 -mb-6">
-                                <img class="h-48 w-28 object-center mx-auto pt-10" src="../<?= $category->image; ?>" />
-                                <!-- <h1 class="absolute  text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">KindaCode.com</h1> -->
-                            </div>
-
+            <div class="container px-5 py-6 mx-auto ">
+                <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 lg:gap-8 -m-4 sm:p-4 gap-2">
+                <?php foreach ($categories as $category) : ?>
+                    <article class=" tile has-background-image" style="background-image: url(../<?= $category->image; ?>);">
+                    <div class="tile-content">
+                        <h2 class="shop-category"><?= strtoupper($category->name); ?></h2>
+                        <div class="divider"></div>
+                        <p>Inner Comfort</p>
+                        <p class="is-italic">Finest products collected amongst the countrie's best artisans.</p>
+                        <div class="products">
+                            268 <span>Products</span>
                         </div>
-
+                        <a href="products.html" class="action">
+                            <span>Discover</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right">
+                                <polyline points="9 18 15 12 9 6"></polyline>
+                            </svg>
+                        </a>
                     </div>
-                    <div class="shadow-lg bg-green-50 text-green-500 text-lg font-bold text-center p-8 rounded-lg">
-                        <?php
-                        $category = array_shift($categories);
-                        ?>
-                        <h1 class="pb-2 text-xl font-thin"><?= strtoupper($category->name); ?></h1>
+                    <!-- Overlay -->
+                    <div class="tile-overlay"></div>
+                    </article>
 
-                        <div class="relative h-20 ">
-                            <img class="h-20 w-28 object-center mx-auto" src="../<?= $category->image; ?>" />
-                            <!-- <h1 class="absolute  text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                                KindaCode.com</h1> -->
-
-                        </div>
-
-                    </div>
-                    <div class=" shadow-lg bg-green-50 text-green-500 text-lg font-bold text-center p-8 rounded-lg row-span-2">
-                        <?php
-                        $category = array_shift($categories);
-                        ?>
-                        <div class="">
-                            <h1 class="pt-6 text-3xl font-thin"><?= strtoupper($category->name); ?></h1>
-
-                            <div class="relative h-48 -mb-6">
-                                <img class="h-48 w-28 object-center mx-auto pt-10" src="../<?= $category->image; ?>" />
-                                <!-- <h1 class="absolute  text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">KindaCode.com</h1> -->
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="shadow-lg bg-green-50 text-green-500 text-lg font-bold text-center p-8 rounded-lg">
-                        <?php
-                        $category = array_shift($categories);
-                        ?>
-                        <h1 class="pb-2 text-xl font-thin"><?= strtoupper($category->name); ?></h1>
-
-                        <div class="relative h-20 ">
-                            <img class="h-20 w-28 object-center mx-auto" src="../<?= $category->image; ?>" />
-                            <!-- <h1 class="absolute  text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                                KindaCode.com</h1> -->
-
-                        </div>
-
-                    </div>
-                    <div class="shadow-lg bg-green-50 text-green-500 text-lg font-bold text-center p-8 rounded-lg col-span-2"> <?php
-                                                                                                                                $category = array_shift($categories);
-                                                                                                                                ?>
-                        <div class="flex">
-                            <div class="relative h-20 ">
-                                <img class="h-20 w-28 object-center mx-auto" src="../<?= $category->image; ?>" />
-                                <!-- <h1 class="absolute  text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                                KindaCode.com</h1> -->
-
-                            </div>
-                            <h1 class=""><?= $category->name; ?></h1>
-                        </div>
-
-                    </div>
-                    <div class="shadow-lg bg-green-50 text-green-500 text-lg font-bold text-center p-8 rounded-lg">
-                        <?php
-                        $category = array_shift($categories);
-                        ?>
-                        <div class="relative h-20 ">
-                            <img class="h-20 w-28 object-center mx-auto" src="../<?= $category->image; ?>" />
-                            <!-- <h1 class="absolute  text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                                KindaCode.com</h1> -->
-
-                        </div>
-                        <h1 class=""><?= $category->name; ?></h1>
-
-                    </div>
-
+                <?php endforeach; ?>
                 </div>
-
-
-
-
-
-                <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 lg:gap-8 -m-4 sm:p-4">
-
-                    <!-- <div class="bg-white p-4 w-full shadow-sm rounded-md">
-                            <a href="shop/earrings/" class="block relative h-64 rounded overflow-hidden">
-                                <img loading="lazy" alt="Chungu Image" class="transform transition duration-500 hover:scale-125 object-cover  w-full h-full block" src="../">
-                            </a>
-                            <div class="mt-4">
-                                <h3 class="text-pink-550 text-xs tracking-widest title-font mb-1">EARRINGS</h3>
-                                <h2 style="font-family: 'Cedarville Cursive', cursive;" class="text-green-550 title-font text-lg font-medium"></h2>
-                                <p class="mt-1 text-pink-550">Ksh></p>
-                            </div>
-                        </div> -->
-
                 </div>
             </div>
         <?php endif; ?>

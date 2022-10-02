b49a753b069231e71ac246a839b470c1d747850a
Peter Munene <peternjega7@gmail.com>
2022-10-02 14:03:43 +0300

    passing tests

--- tests/Unit/DatabaseTest.php
+++ tests/Unit/DatabaseTest.php
@@ -8,16 +8,19 @@ use Chungu\Core\Database\Connection;
 
 require __DIR__ . '/../../Core/helpers.php';
 
+define('ENV', 'development');
+
 class DatabaseTest extends TestCase {
 
     protected function setUp(): void {
 
         parent::setUp();
         $this->config_file  =  __DIR__ . "/../../config.php";
-        $this->config_details =  require_once($this->config_file);
+        $this->config_details = require $this->config_file;
     }
 
     public function test_if_there_is_a_config_for_database() {
+      
         $this->assertFileExists($this->config_file);
         $this->assertArrayHasKey("mysql", $this->config_details, "Config does not contain 'mysql' details");
         $this->assertArrayHasKey("sqlite", $this->config_details, "Config does not contain 'sqlite' details");

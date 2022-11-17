aeb68255aaa94e9665cfbf951fa85593ab901e2c
munenepeter <peternjega7@gmail.com>
2022-11-17 19:45:35 +0300

    bugs with flowbite

--- Views/test.view.php
+++ Views/test.view.php
@@ -10,7 +10,6 @@ use Chungu\Core\Mantle\Paginator; ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
-    <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
@@ -164,7 +163,7 @@ use Chungu\Core\Mantle\Paginator; ?>
 
 
                                                     <!-- Main modal -->
-                                                    <div id="readModal<?= $product->id ?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                                                    <div id="readModal<?= $product->id ?>" data-modal-placement="center" tabindex="-1"  aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
                                                         <div class="relative p-4 w-full max-w-xl h-full md:h-auto">
                                                             <!-- Modal content -->
                                                             <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
@@ -224,7 +223,7 @@ use Chungu\Core\Mantle\Paginator; ?>
 
 
                                                     <!-- Main modal -->
-                                                    <div id="updateModal<?= $product->id ?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                                                    <div id="updateModal<?= $product->id ?>" data-modal-placement="center" tabindex="-1"  aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
                                                         <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
                                                             <!-- Modal content -->
                                                             <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
@@ -317,7 +316,7 @@ use Chungu\Core\Mantle\Paginator; ?>
                                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                                         </svg>
                                                         <!-- Main modal -->
-                                                        <div id="deleteModal<?= $product->id ?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                                                        <div id="deleteModal<?= $product->id ?>" data-modal-placement="center" tabindex="-1"   aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
                                                             <div class="relative p-4 w-full max-w-md h-full md:h-auto">
                                                                 <!-- Modal content -->
                                                                 <div class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
@@ -361,6 +360,7 @@ use Chungu\Core\Mantle\Paginator; ?>
     </div>
 
 
+    <script defer src="https://unpkg.com/flowbite@1.5.2/dist/flowbite.js"></script>
 </body>
 
 </html>

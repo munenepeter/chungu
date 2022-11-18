09c6c8789b8fc7de202f345aabdfb6cf4d7d3840
munenepeter <peternjega7@gmail.com>
2022-11-18 12:43:15 +0300

    little db refactor

--- Controllers/ApiController.php
+++ Controllers/ApiController.php
@@ -3,27 +3,27 @@
 namespace Chungu\Controllers;
 
 use Chungu\Models\Category;
-use Chungu\Models\Product;  
+use Chungu\Models\Product;
 
-class ApiController extends Controller{
+class ApiController extends Controller {
+    public function __construct() {
+        header('Content-Type: application/json; charset=utf-8');
+    }
     public function index() {
         $products =  Category::all();
         echo json_encode($products);
-        
     }
     public function all() {
-        $products = Product::all();
+        $products = Product::query("SELECT p.id, p.name, p.color,p.price, p.quantity, p.status, p.image, p.created_at, p.updated_at, c.id as category_id, c.name as categoryName, c.image as categoryImage FROM products p INNER JOIN categories c WHERE p.`category_id` = c.`id`;");
         echo json_encode($products);
-        
     }
     public function category($category) {
         $$category = [
             $category => $this->getProducts($category)
         ];
         echo json_encode($$category);
-        
     }
-    public function dashboard(){
+    public function dashboard() {
         $data = [];
         foreach (Category::all() as $category) {
             $data[$category->name]['name'] = $category->name;
--- Core/Database/sqlite/sqlite-schema.sql
+++ Core/Database/sqlite/sqlite-schema.sql
@@ -51,9 +51,7 @@ CREATE TABLE `categories` (
   `created_at` datetime,
   `updated_at` datetime
 );
-DROP TABLE  `categories`;
 
---
   INSERT INTO `categories` (`id`, `name`,  `image`, `created_at`, `updated_at`) VALUES
 ('cat-5ece4797eaf5e', 'earrings',  '/static/imgs/categories\\earring.svg', '2022-06-29 10:16:47.000000', '2022-06-29 11:10:47.000000'),
 ('cat-62962f89b29b3', 'rings',  '/static/imgs/categories\\ring.svg', '2022-06-11 10:16:47.000000', '2022-06-01 10:26:47.000000'),
@@ -62,6 +60,28 @@ DROP TABLE  `categories`;
 ('cat-62962ffec3fe7', 'belts',  '/static/imgs/categories\\belt.svg', '2022-06-20 10:16:47.000000', '2022-06-16 10:26:47.000000'),
 ('cat-62ac4aec68b30', 'perfume',  '/static/imgs/categories\\perfume.svg', '2022-06-13 10:16:47.000000', '2022-06-26 10:16:47.000000'),
 ('cat-629785244dce4', 'necklaces',  '/static/imgs/categories\\necklace.svg', '2022-06-23 10:16:47.000000', '2022-06-23 10:16:47.000000');
+
+DROP TABLE  `categories`;
+
+
+CREATE TABLE `brands` (
+  `id` varchar(300) PRIMARY KEY NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `created_at` datetime,
+  `updated_at` datetime
+);
+
+
+DROP TABLE  `brands`;
+--
+  INSERT INTO `brands` (`id`, `name`, `created_at`, `updated_at`) VALUES
+('bra-5ece4797eaf5e', 'fashona', '2022-06-29 10:16:47.000000', '2022-10-29 11:10:47.000000'),
+('bra-62962f89b29b3', 'maxima', '2022-06-11 10:16:47.000000', '2022-10-01 10:26:47.000000'),
+('bra-62962f98473f6', 'nova', '2022-06-22 10:16:47.000000', '2022-10-27 10:16:47.000000'),
+('bra-62962fb1d0ca6', 'bold', '2022-06-29 10:16:47.000000', '2022-10-24 10:16:47.000000'),
+('bra-62962ffec3fe7', 'samba', '2022-06-20 10:16:47.000000', '2022-10-16 10:26:47.000000'),
+('bra-62ac4aec68b30', 'koko', '2022-06-13 10:16:47.000000', '2022-10-26 10:16:47.000000'),
+('bra-629785244dce4', 'kenya', '2022-06-23 10:16:47.000000', '2022-10-23 10:16:47.000000');
 
 
 CREATE TABLE `sources` (
@@ -76,3 +96,12 @@ CREATE TABLE `sources` (
 );
 
 DELETE FROM `sources` WHERE `id` < 3;
+
+
+SELECT
+ p.id, p.name, p.color,p.price, p.quantity, p.status, p.image, p.created_at, p.updated_at,
+ c.id as category_id, c.name as categoryName, c.image as categoryImage
+ FROM products p 
+ INNER JOIN categories c 
+ WHERE 
+ p.`category_id` = c.`id`;

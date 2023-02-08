8147514e189a990b9a7c257a35cd1055582dfa93
munenepeter <peternjega7@gmail.com>
2023-02-08 17:34:15 +0300

    new template

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,27 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Chungu Collections') }}</title>
-
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
-    <!-- Scripts -->
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <script src="{{ mix('js/app.js') }}" defer></script>
-</head>
-
-<body>
+<x-template-layout>
     <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700 pr-4">
         <div class="flex flex-wrap justify-between items-center mx-auto  p-1">
             <a href="/" class="p-2 block hover:text-orange-550">
-                <p style="font-family: 'Cedarville Cursive', cursive;" class="ml-2 hover:text-orange-550 text-4xl font-black dark:text-white">Chungu</p>
-                <p class="text-lg font-bold whitespace-nowrap dark:text-white hover:text-orange-550">COLLECTIONS</p>
+                <p style="font-family: 'Cedarville Cursive', cursive;" class="text-green-550 ml-2 hover:text-orange-550 text-4xl font-black ">Chungu</p>
+                <p class="text-lg font-bold whitespace-nowrap text-green-550 hover:text-orange-550">COLLECTIONS</p>
             </a>
             <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
@@ -35,21 +17,21 @@
             <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
                 <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium items-center">
                     <li>
-                        <a href="/#about" class="text-md block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-orange-550 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                        <a href="/#about" class="text-green-550 text-md block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-orange-550 md:p-0 ">About</a>
                     </li>
                     <li>
-                        <a href="/#testimonials" class="text-md block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-orange-550 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Testimonials</a>
+                        <a href="/#testimonials" class="text-green-550 text-md block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-orange-550 md:p-0 ">Testimonials</a>
                     </li>
                     @if (Route::has('login'))
                     <li>
                         @auth
-                        <a href="{{ url('/dashboard') }}" class="text-md block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-orange-550 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}" class="text-green-550 text-md block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-orange-550 md:p-0 ">Dashboard</a>
                         @else
-                        <a href="{{ route('login') }}" class="text-md block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-orange-550 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Log in</a>
+                        <a href="{{ route('login') }}" class="text-green-550 text-md block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-orange-550 md:p-0 ">Log in</a>
                     </li>
                     <li>
                         @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-md block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-orange-550 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Register</a>
+                        <a href="{{ route('register') }}" class="text-green-550 ml-4 text-md block py-2 pr-4 pl-3  border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 hover:text-orange-550 md:p-0 ">Register</a>
                         @endif
                         @endauth
                     </li>
@@ -62,40 +44,40 @@
     <div class="font-sans text-gray-900 antialiased">
         {{ $slot }}
     </div>
-
     <footer id="footer" class="border-t bg-gray-50 left-50 w-full fixed bottom-0">
-        <div class=" pt-5">
-            <div class="max-w-screen-lg px-4 sm:px-6 text-gray-800 sm:grid md:grid-cols-4 sm:grid-cols-2 mx-auto">
-                <div class="p-5">
-                    <p style="font-family: 'Cedarville Cursive', cursive;" class="text-green-550 ml-2 hover:text-orange-550 text-4xl font-black dark:text-white">Chungu</p>
-                    <p class="text-green-550 text-lg font-bold whitespace-nowrap dark:text-white hover:text-orange-550">COLLECTIONS</p>
+        <div class="px-4">
+            <div class="flex justify-between">
+                <div class="p-2">
+                    <p style="font-family: 'Cedarville Cursive', cursive;" class="text-green-550 ml-2 hover:text-orange-550 text-4xl font-black ">Chungu</p>
+                    <p class="text-green-550 text-lg font-bold whitespace-nowrap hover:text-orange-550">COLLECTIONS</p>
+                    <span class="text-xs">Admin Login</span>
                 </div>
 
-                <div class="p-5">
-                    <div class="text-sm uppercase text-orange-550 font-bold">Useful Links</div>
-                    <a class="my-3 block text-green-550 hover:underline" href="/shop">Shop <span class="text-teal-600 text-xs p-1"></span></a>
-                    <a class="my-3 block text-green-550 hover:underline" href="/cart">Cart<span class="text-teal-600 text-xs p-1"></span></a>
-                    <a class="my-3 block text-green-550 hover:underline" href="/#">Support <span class="text-teal-600 text-xs p-1">New</span></a>
-                </div>
-                <div class="p-5">
-                    <div class="text-sm uppercase text-orange-550 font-bold">Support</div>
-                    <a class="my-3 block text-green-550 hover:underline" href="/help-center">Help Center <span class="text-teal-600 text-xs p-1"></span></a>
-                    <a class="my-3 block text-green-550 hover:underline" href="/privacy-policy">Privacy Policy <span class="text-teal-600 text-xs p-1"></span></a>
-                    <a class="my-3 block text-green-550 hover:underline" href="/faqs">FAQs<span class="text-teal-600 text-xs p-1"></span></a>
-                </div>
-                <div class="p-5">
-                    <div class="text-sm uppercase text-orange-550 font-bold">Contact us</div>
-                    <a class="my-3 block text-green-550 hover:underline" href="/#">+2547 426 41376<br>+2547 980 55244<br> Nairobi. <span class="text-teal-600 text-xs p-1"></span></a>
-                    <a class="my-3 block text-teal-600 hover:underline" href="mailto:info@chungu.co.ke ">info@chungu.co.ke <span class="text-teal-600 text-xs p-1"></span></a>
-                </div>
-
+                <article class="sm:grid md:grid-cols-3 sm:grid-cols-2 p-2">
+                    <div>
+                        <div class="text-sm uppercase text-orange-550 font-bold">Useful Links</div>
+                        <a class="my-3 block text-green-550 hover:underline" href="/shop">Shop <span class="text-teal-600 text-xs p-1"></span></a>
+                        <a class="my-3 block text-green-550 hover:underline" href="/cart">Cart<span class="text-teal-600 text-xs p-1"></span></a>
+                        <a class="my-3 block text-green-550 hover:underline" href="/#">Support <span class="text-teal-600 text-xs p-1">New</span></a>
+                    </div>
+                    <div>
+                        <div class="text-sm uppercase text-orange-550 font-bold">Support</div>
+                        <a class="my-3 block text-green-550 hover:underline" href="/help-center">Help Center <span class="text-teal-600 text-xs p-1"></span></a>
+                        <a class="my-3 block text-green-550 hover:underline" href="/privacy-policy">Privacy Policy <span class="text-teal-600 text-xs p-1"></span></a>
+                        <a class="my-3 block text-green-550 hover:underline" href="/faqs">FAQs<span class="text-teal-600 text-xs p-1"></span></a>
+                    </div>
+                    <div>
+                        <div class="text-sm uppercase text-orange-550 font-bold">Contact us</div>
+                        <a class="my-3 block text-green-550 hover:underline" href="/#">+2547 426 41376<br>+2547 980 55244<br> Nairobi. <span class="text-teal-600 text-xs p-1"></span></a>
+                        <a class="my-3 block text-teal-600 hover:underline" href="mailto:info@chungu.co.ke ">info@chungu.co.ke <span class="text-teal-600 text-xs p-1"></span></a>
+                    </div>
+                </article>
             </div>
         </div>
 
-        <div class=" pt-2">
-            <div class="flex pb-5 px-3 m-auto pt-5 border-t text-gray-800 text-sm flex-col
-      max-w-screen-lg items-center">
-                <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+        <div class="mt-2">
+            <div class="flex px-3 m-auto border-t text-gray-800 text-sm flex-col max-w-screen-lg items-center">
+                <div class="md:flex-auto md:flex-row-reverse my-2 flex-row flex">
                     <a target="_blank" rel="noopener noreferrer" href="https://www.pinterest.com/chungucollections/" class="w-6 mx-1" title="Pinterest">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-green-550 hover:text-orange-550" fill="currentColor" viewBox="0 0 32 32" class="w-4 h-4">
                             <path d="M16.021 0c-8.828 0-15.984 7.156-15.984 15.984 0 6.771 4.214 12.552 10.161 14.88-0.141-1.266-0.266-3.203 0.052-4.583 0.292-1.25 1.875-7.943 1.875-7.943s-0.479-0.964-0.479-2.375c0-2.219 1.292-3.88 2.891-3.88 1.365 0 2.026 1.021 2.026 2.25 0 1.37-0.87 3.422-1.323 5.323-0.38 1.589 0.797 2.885 2.365 2.885 2.839 0 5.026-2.995 5.026-7.318 0-3.813-2.75-6.49-6.677-6.49-4.547 0-7.214 3.417-7.214 6.932 0 1.375 0.526 2.854 1.188 3.651 0.13 0.161 0.146 0.302 0.109 0.464-0.12 0.5-0.391 1.599-0.443 1.818-0.073 0.297-0.229 0.359-0.536 0.219-1.99-0.922-3.245-3.839-3.245-6.193 0-5.036 3.667-9.672 10.563-9.672 5.542 0 9.854 3.958 9.854 9.229 0 5.516-3.474 9.953-8.307 9.953-1.62 0-3.141-0.839-3.677-1.839l-1 3.797c-0.359 1.391-1.339 3.135-2 4.193 1.5 0.458 3.078 0.714 4.734 0.714 8.813 0 15.979-7.151 15.979-15.984 0-8.828-7.167-15.979-15.979-15.979z"></path>
@@ -122,17 +104,10 @@
                             <path d="M31.937 6.093c-1.177 0.516-2.437 0.871-3.765 1.032 1.355-0.813 2.391-2.099 2.885-3.631-1.271 0.74-2.677 1.276-4.172 1.579-1.192-1.276-2.896-2.079-4.787-2.079-3.625 0-6.563 2.937-6.563 6.557 0 0.521 0.063 1.021 0.172 1.495-5.453-0.255-10.287-2.875-13.52-6.833-0.568 0.964-0.891 2.084-0.891 3.303 0 2.281 1.161 4.281 2.916 5.457-1.073-0.031-2.083-0.328-2.968-0.817v0.079c0 3.181 2.26 5.833 5.26 6.437-0.547 0.145-1.131 0.229-1.724 0.229-0.421 0-0.823-0.041-1.224-0.115 0.844 2.604 3.26 4.5 6.14 4.557-2.239 1.755-5.077 2.801-8.135 2.801-0.521 0-1.041-0.025-1.563-0.088 2.917 1.86 6.36 2.948 10.079 2.948 12.067 0 18.661-9.995 18.661-18.651 0-0.276 0-0.557-0.021-0.839 1.287-0.917 2.401-2.079 3.281-3.396z"></path>
                         </svg>
                     </a>
-
-
                 </div>
-                <div class="my-5 text-center">&copy; 2020 - 2022 All rights reserved | Chungu Collections</div>
+                <div class="text-center">&copy; 2020 - {{date('Y')}}, All rights reserved | Chungu Collections</div>
             </div>
         </div>
     </footer>
 
-</body>
-
-</html>
-</body>
-
-</html>
+    </x-template-layout>

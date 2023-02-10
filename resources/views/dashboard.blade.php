a3f3f2d037c4cb53fce27b1a791d007d41a293dd
munenepeter <peternjega7@gmail.com>
2023-02-10 15:46:29 +0300

    reformat

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-4 lg:px-6">
+        <div class="max-w-7xl mx-auto sm:px-2 lg:px-2">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg"> <x-jet-welcome /> </div>
         </div>
     </div>

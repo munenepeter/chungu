f0453f1c8bb4db0f39f48e9b7afdc7078413a558
munenepeter <peternjega7@gmail.com>
2023-02-20 18:14:10 +0300

    done with dashboard for now

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -161,10 +161,10 @@
             </div>
 
             <a href="https://laravel.com/docs">
-                <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700 dark:text-indigo-300">
+                <div class="mt-3 flex items-center text-sm font-semibold text-orange-550">
                     <div>View all products</div>
 
-                    <div class="ml-1 text-indigo-500 dark:text-indigo-200">
+                    <div class="ml-1 text-orange-550">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                         </svg>
@@ -174,7 +174,7 @@
         </div>
     </div>
 
-    <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+    <!-- <div class="p-6 border-t border-gray-200 dark:border-gray-700">
         <div class="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
@@ -202,5 +202,5 @@
                 Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started what matters most: building your application.
             </div>
         </div>
-    </div>
+    </div> -->
 </div>

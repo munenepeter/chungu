b0b93d183bb3ee1c41db00cc6a7722a69ee17510
munenepeter <peternjega7@gmail.com>
2023-02-25 22:24:01 +0300

    update btns with svgs

--- resources/views/livewire/sources/sources.blade.php
+++ resources/views/livewire/sources/sources.blade.php
@@ -71,9 +71,13 @@
                                 {{$source->notes}}
                             </td>
                             <td class="px-6 py-4">
-                                <button wire:click="editSource({{$source->id}})" class="btn btn-primary btn-sm">Edit</button>
-                                <button wire:onclick="deleteSource({{$source->id}})" class="btn btn-danger btn-sm">Delete</button>
-                                <x-jet-danger-button></x-jet-danger-button>
+                                <button wire:click="editSource({{$source->id}})"><svg xmlns="http://www.w3.org/2000/svg" class="hover:text-green-600 text-green-400 w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                    </svg></button>
+                                <button wire:onclick="deleteSource({{$source->id}})"><svg xmlns="http://www.w3.org/2000/svg" class="cursor-pointer hover:text-red-600 text-red-400 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.8612.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                    </svg></button>
+
                             </td>
                         </tr>
                         @endforeach

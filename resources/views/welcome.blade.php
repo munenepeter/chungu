032f65eb6ada5eec14ca5b5426bfbbe92591c839
munenepeter <peternjega7@gmail.com>
2023-03-08 20:12:27 +0300

    add about section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,8 +18,7 @@
                 &#8592;
             </button>
 
-            @for($i = 1; $i<5; $i++) 
-            <article class="flex items-center justify-center flex-col gap-2 p-2">
+            @for($i = 1; $i<5; $i++) <article class="flex items-center justify-center flex-col gap-2 p-2">
                 <img src="https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=600" class="w-24 h-24 rounded-full object-cover transition duration-200 hover:scale-110">
 
                 <div class="my-2 text-orange-550 text-lg font-semibold">Category</div>
@@ -34,10 +33,33 @@
         </section>
 
 
-
-        <a href="/offers" class="my-4 ml-96 text-xl py-2 lg:py-3 px-3 md:px-6 font-medium text-center text-green-550 rounded-lg focus:ring-4 hover:bg-green-550 hover:text-white focus:outline-none focus:ring-green-300 border-2 border-green-550">
+        <div class="ml-96 my-12">
+            <a href="/shop" class="text-xl py-2 lg:py-3 px-3 md:px-6 font-medium text-center text-green-550 rounded-lg focus:ring-4 hover:bg-green-550 hover:text-white focus:outline-none focus:ring-green-300 border-2 border-green-550">
                 More Offers
             </a>
+        </div>
+
+        <section class="mt-4" id="about" >
+            <center>
+                <h5 style="font-family: 'Cedarville Cursive', cursive;" class="my-8 text-3xl font-black tracking-loose text-pink-550 dark:text-white">About Chungu Collections</h5>
+            </center>
+            <div class="relative h-64 w-full flex items-end justify-start text-left bg-cover bg-center" style="background-image:url(https://images.unsplash.com/photo-1551970634-747846a548cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JlZW4lMjBsZWFmc3xlbnwwfHwwfHw%3D&w=1000&q=80);">
+                <div class="absolute top-0 mt-20 right-0 bottom-0 left-0 bg-gradient-to-b from-transparent to-gray-900"></div>
+                <main class="p-5 z-10 mx-auto">
+                    <a href="https://chungu.com" class="block text-white hover:text-pink-550 ">
+                        <p style="font-family: 'Cedarville Cursive', cursive;" class="hover:text-pink-550 text-8xl font-black ">Chungu</p>
+                        <p class="mt-4 text-4xl font-bold whitespace-nowrap dark:text-white hover:text-pink-550">COLLECTIONS</p>
+                    </a>
+                </main>
+            </div>
+            <div class="my-10  ">
+                <div class="w-full lg:w-1/2 mx-auto p-4 md:p-2">
+                    <p class="leading-loose text-center font-semibold text-md md:text-lg">We offer a wide range of beautiful and timeless pieces to suit any style. Whether you're looking for a special gift for a loved one or a treat for yourself, you'll find what you're looking for here. Our collection includes necklaces, bracelets, earrings, and rings crafted from high-quality materials, including gold, silver, and diamonds. We're dedicated to providing our customers with the best shopping experience possible, and that's why we offer free shipping, easy returns, and excellent customer service. Start browsing our collection today and discover the perfect piece of jewelry to enhance your style.</p>
+                </div>
+            </div>
+        </section>
+
+
 
 
 

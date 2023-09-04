d2eb5b3655045ccfd08616ff31fd4b1fd71a1ca1
munenepeter <peternjega7@gmail.com>
2023-09-04 19:21:13 +0300

    to install spatie media

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Product extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
+
+    /**
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'featured' => 'boolean',
+        'is_visible' => 'boolean',
+        'backorder' => 'boolean',
+        'requires_shipping' => 'boolean',
+        'published_at' => 'date',
+    ];
+
+    public function brand() {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class)->withTimestamps();
+    }
 }

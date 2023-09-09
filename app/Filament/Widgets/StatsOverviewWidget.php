71f6ba5c80b6d87a50d552258bbc4b7d2647c8b7
munenepeter <peternjega7@gmail.com>
2023-09-09 15:37:33 +0300

    update currency

--- app/Filament/Widgets/StatsOverviewWidget.php
+++ app/Filament/Widgets/StatsOverviewWidget.php
@@ -12,18 +12,18 @@ class StatsOverviewWidget extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Revenue', '$192.1k')
-                ->description('32k increase')
+            Stat::make('Revenue', 'Ksh19k')
+                ->description('2k increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('success'),
-            Stat::make('New customers', '1340')
+            Stat::make('New customers', '40')
                 ->description('3% decrease')
                 ->descriptionIcon('heroicon-m-arrow-trending-down')
                 ->chart([17, 16, 14, 15, 14, 13, 12])
                 ->color('danger'),
-            Stat::make('New orders', '3543')
-                ->description('7% increase')
+            Stat::make('New orders', '13')
+                ->description('1% increase')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->chart([15, 4, 10, 2, 12, 4, 12])
                 ->color('success'),

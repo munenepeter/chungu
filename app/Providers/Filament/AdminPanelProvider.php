6d6c58a0a9f891b4ce30a39a6cce69f10b4604d2
munenepeter <peternjega7@gmail.com>
2023-09-09 11:00:21 +0300

    add admin favicon

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->favicon(asset('favicon-admin.svg'))
             ->colors([
                 'primary' => Color::hex('#799649'),
             ])

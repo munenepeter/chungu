52cd998e45af4a44b3e03795521a1749f4f43dba
munenepeter <peternjega7@gmail.com>
2023-12-25 13:46:52 +0300

    new changes

--- resources/views/livewire/shop/collections.blade.php
+++ resources/views/livewire/shop/collections.blade.php
@@ -1,25 +1,118 @@
-<main>
-<div class="p-5 sm:p-8">
-    <div class="columns-1 gap-5 sm:columns-2 sm:gap-8 md:columns-3 lg:columns-4 [&>img:not(:first-child)]:mt-8">
-      
-      
-      
-        <img src="https://source.unsplash.com/bYuI23mnmDQ"/>
-        <img src="https://source.unsplash.com/Nllx4R-2c3o"/>
-        <img src="https://source.unsplash.com/lp40q07DIe0"/>
-        <img src="https://source.unsplash.com/wfalq01jJuU"/>
-        <img src="https://source.unsplash.com/rMHNK_skwwU"/>
-        <img src="https://source.unsplash.com/WBMjuGpbrCQ"/>
-        <img src="https://source.unsplash.com/nCUZ5BYBL_o"/>
-        <img src="https://source.unsplash.com/3u4fzMQZhjc"/>
-        <img src="https://source.unsplash.com/haOIqIPSwps"/>
-        <img src="https://source.unsplash.com/3UrYD7NNVxk"/>
-        <img src="https://source.unsplash.com/fm1JKDItlVM"/>
-        <img src="https://source.unsplash.com/qPpq1EVs8vw"/>
-        <img src="https://source.unsplash.com/xRyL63AwZFE"/>
-        <img src="https://source.unsplash.com/XeNKWTiCPNw"/>
-        <img src="https://source.unsplash.com/DFt3T5r_4FE"/>
-        <img src="https://source.unsplash.com/Ebwp2-6BG8E"/>
+<div class="bg-white dark:bg-gray-800 py-6 sm:py-8 lg:py-12">
+    <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div class="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
+            <div class="flex items-center gap-12">
+                <h2 class="text-2xl font-bold text-gray-800 lg:text-3xl dark:text-white">Gallery</h2>
+
+                <p class="hidden max-w-screen-sm text-gray-500 dark:text-gray-300 md:block">
+                    This is a section of some simple filler text,
+                    also known as placeholder text. It shares some characteristics of a real written text.
+                </p>
+            </div>
+
+            <a href="#"
+                class="inline-block rounded-lg border bg-white dark:bg-gray-700 dark:border-none px-4 py-2 text-center text-sm font-semibold text-gray-500 dark:text-gray-200 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base">
+                More
+            </a>
+        </div>
+
+        <div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 relative">
+            <div class="grid grid-cols-1 sm:grid-cols-12 gap-5">
+
+                <div class="sm:col-span-5">
+                    <a href="#">
+                        <div class="bg-cover text-center overflow-hidden"
+                            style="min-height: 300px; background-image: url('https://api.time.com/wp-content/uploads/2020/07/never-trumpers-2020-election-01.jpg?quality=85&amp;w=1201&amp;h=676&amp;crop=1')"
+                            title="Woman holding a mug">
+                        </div>
+                    </a>
+                    <div
+                        class="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+                        <div class="">
+                            <a href="#"
+                                class="text-xs text-indigo-600 uppercase font-medium hover:text-gray-900 transition duration-500 ease-in-out">
+                                Election
+                            </a>
+                            <a href="#"
+                                class="block text-gray-900 font-bold text-2xl mb-2 hover:text-indigo-600 transition duration-500 ease-in-out">Revenge
+                                of the Never Trumpers</a>
+                            <p class="text-gray-700 text-base mt-2">Meet the Republican dissidents fighting to push
+                                Donald Trump
+                                out of office—and reclaim their party</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="sm:col-span-7 grid grid-cols-2 lg:grid-cols-3 gap-5">
+                    <div class="">
+                        <a href="#">
+                            <div class="h-40 bg-cover text-center overflow-hidden"
+                                style="background-image: url('https://api.time.com/wp-content/uploads/2020/07/president-trump-coronavirus-election.jpg?quality=85&amp;w=364&amp;h=204&amp;crop=1')"
+                                title="Woman holding a mug">
+                            </div>
+                        </a>
+                        <a href="#"
+                            class="text-gray-900 inline-block font-semibold text-md my-2 hover:text-indigo-600 transition duration-500 ease-in-out">Trump
+                            Steps Back Into Coronavirus Spotlight</a>
+                    </div>
+                    <div class="">
+                        <a href="#">
+                            <div class="h-40 bg-cover text-center overflow-hidden"
+                                style="background-image: url('https://api.time.com/wp-content/uploads/2020/06/GettyImages-1222922545.jpg?quality=85&amp;w=364&amp;h=204&amp;crop=1')"
+                                title="Woman holding a mug">
+                            </div>
+                        </a>
+                        <a href="#"
+                            class="text-gray-900 inline-block font-semibold text-md my-2 hover:text-indigo-600 transition duration-500 ease-in-out">How
+                            Trump's Mistakes Became Biden's Big Breaks</a>
+                    </div>
+                    <div class="">
+                        <a href="#">
+                            <div class="h-40 bg-cover text-center overflow-hidden"
+                                style="background-image: url('https://api.time.com/wp-content/uploads/2020/07/American-Flag.jpg?quality=85&amp;w=364&amp;h=204&amp;crop=1')"
+                                title="Woman holding a mug">
+                            </div>
+                        </a>
+                        <a href="#"
+                            class="text-gray-900 inline-block font-semibold text-md my-2 hover:text-indigo-600 transition duration-500 ease-in-out">Survey:
+                            Many Americans 'Dissatisfied' With U.S.</a>
+                    </div>
+                    <div class="">
+                        <a href="#">
+                            <div class="h-40 bg-cover text-center overflow-hidden"
+                                style="background-image: url('https://api.time.com/wp-content/uploads/2020/06/GettyImages-1222922545.jpg?quality=85&amp;w=364&amp;h=204&amp;crop=1')"
+                                title="Woman holding a mug">
+                            </div>
+                        </a>
+                        <a href="#"
+                            class="text-gray-900 inline-block font-semibold text-md my-2 hover:text-indigo-600 transition duration-500 ease-in-out">How
+                            Trump's Mistakes Became Biden's Big Breaks</a>
+                    </div>
+                    <div class="">
+                        <a href="#">
+                            <div class="h-40 bg-cover text-center overflow-hidden"
+                                style="background-image: url('https://api.time.com/wp-content/uploads/2020/07/American-Flag.jpg?quality=85&amp;w=364&amp;h=204&amp;crop=1')"
+                                title="Woman holding a mug">
+                            </div>
+                        </a>
+                        <a href="#"
+                            class="text-gray-900 inline-block font-semibold text-md my-2 hover:text-indigo-600 transition duration-500 ease-in-out">Survey:
+                            Many Americans 'Dissatisfied' With U.S.</a>
+                    </div>
+                    <div class="">
+                        <a href="#">
+                            <div class="h-40 bg-cover text-center overflow-hidden"
+                                style="background-image: url('https://api.time.com/wp-content/uploads/2020/07/president-trump-coronavirus-election.jpg?quality=85&amp;w=364&amp;h=204&amp;crop=1')"
+                                title="Woman holding a mug">
+                            </div>
+                        </a>
+                        <a href="#"
+                            class="text-gray-900 inline-block font-semibold text-md my-2 hover:text-indigo-600 transition duration-500 ease-in-out">Trump
+                            Steps Back Into Coronavirus Spotlight</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 </div>
-</main>
